2008eb68464cf6a876a00fe4f8e3cf30ba434b9f
songzhw <songzhw2012@gmail.com>
2015-08-21 16:03:22 +0800

    Kotlin A16 : Range and Cast

--- src/cn/song/abbasic/Array02.kt
+++ src/cn/song/abbasic/Array02.kt
@@ -4,18 +4,18 @@ package cn.song.abbasic
  * Created by hzsongzhengwang on 2015/8/18.
  */
 
-fun main(args: Array<String>) {
-    val ary1 = arrayOf(1,2,3,"4")
-    ary1.forEach{ println(it)} //=> 1,2,3,4
-//    ary1.forEach { println(it+1) }  // error!
-
-    // 之所以声明类型，是因为arrayOfNulls要求指定数组存储的数据类型
-    var ary2 : Array<Int?> = arrayOfNulls(5)
-
-    var ary3  = Array(5, {(it * it ).toString()})  //=> 0 1 4 9 16
-
-    var ary4 = intArrayOf(1,2,3)   // int array
-    var ary5 = arrayListOf("1","2", "3")  // string array
-
-
-}
+//fun main(args: Array<String>) {
+//    val ary1 = arrayOf(1,2,3,"4")
+//    ary1.forEach{ println(it)} //=> 1,2,3,4
+////    ary1.forEach { println(it+1) }  // error!
+//
+//    // 之所以声明类型，是因为arrayOfNulls要求指定数组存储的数据类型
+//    var ary2 : Array<Int?> = arrayOfNulls(5)
+//
+//    var ary3  = Array(5, {(it * it ).toString()})  //=> 0 1 4 9 16
+//
+//    var ary4 = intArrayOf(1,2,3)   // int array
+//    var ary5 = arrayListOf("1","2", "3")  // string array
+//
+//
+//}
---
+++ src/cn/song/abbasic/Range04.kt
@@ -0,0 +1,58 @@
+package cn.song.abbasic
+
+/**
+ * Created by hzsongzhengwang on 2015/8/21.
+ */
+
+// 1. return true/false,  for "if(.in.)"
+// 2. for "for( in )"
+
+//fun main(args: Array<String>) {
+//    // Checking if value of comparable is in range. Optimized for number primitives.
+////    if (i in 1..10) println(i)
+////
+////    if (x in 1.0..3.0) println(x)
+////
+////    if (str in "island".."isle") println(str)
+//
+//    // Iterating over arithmetical progression of numbers. Optimized for number primitives (as indexed for-loop in Java).
+//    for (i in 1..4) print(i) // prints "1234"
+//
+//    for (i in 4..1) print(i) // prints nothing
+//
+//    for (i in 4 downTo 1) print(i) // prints "4321"
+//
+//    for (i in 1..4 step 2) print(i) // prints "13"
+//
+//    for (i in (1..4).reversed()) print(i) // prints "4321"
+//
+//    for (i in (1..4).reversed() step 2) print(i) // prints "42"
+//
+//    for (i in 4 downTo 1 step 2) print(i) // prints "42"
+//
+//    for (x in 1.0..2.0) print("$x ") // prints "1.0 2.0 "
+//
+//    for (x in 1.0..2.0 step 0.3) print("$x ") // prints "1.0 1.3 1.6 1.9 "
+//
+//    for (x in 2.0 downTo 1.0 step 0.3) print("$x ") // prints "2.0 1.7 1.4 1.1 "
+//
+//    //for (str in "island".."isle") println(str) // error: string range cannot be iterated over
+//
+//
+//
+//    //utility funcitons
+//    // rangeTo()
+//    // downTo()
+//    // reversed()
+//    // step()
+//    for (i in 1..4 step 2) print(i) // prints "1  3"
+//
+//    for (i in 4 downTo 1 step 2) print(i) // prints "4  2"
+//
+//    for (i in 1.0..2.0 step 0.3) print("$i ") // prints "1.0 1.3 1.6 1.9 "
+//
+//
+//
+//
+//
+//}
