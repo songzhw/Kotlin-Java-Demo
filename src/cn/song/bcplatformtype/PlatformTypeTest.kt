9eca9df10b1f65452f20e7bb07a4f52ed0197c72
songzhw <songzhw2012@gmail.com>
2015-09-02 16:22:03 +0800

    exception & platform type

---
+++ src/cn/song/bcplatformtype/PlatformTypeTest.kt
@@ -0,0 +1,20 @@
+package cn.song.bcplatformtype
+
+import java.util.*
+
+/**
+ * Created by hzsongzhengwang on 2015/9/2.
+ */
+fun test(){
+    val list = ArrayList<String>()
+    list.add("one")
+    val item = list.get(0)
+    var notnull : String = item
+
+
+}
+
+
+fun main(args: Array<String>) {
+    test()
+}
