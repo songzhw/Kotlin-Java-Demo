589ca64874d9d2f4aa889fba565422356ac5d492
songzhw <songzhw2012@gmail.com>
2015-09-15 18:07:34 +0800

    a little more complex builder in Kotlin

--- src/cn/song/ccBuilder/MyHtmlBuilder.kt
+++ src/cn/song/ccBuilder/MyHtmlBuilder.kt
@@ -11,5 +11,29 @@ fun html(init: Html.()-> Unit) : Html {
 
 
 class Html(){
+    var children = arrayListOf<Any>()
 
+    fun head(init: Head.()->Unit) : Head{
+        var head = Head()
+        head.init()
+        children.add(head)
+        return head
+    }
+
+    override fun toString(): String {
+        var sb = StringBuilder()
+        sb.append("<html>\n")
+        children.forEach {
+            sb.append("    ${it.toString()}")
+        }
+        sb.append("\n")
+        sb.append("</html>\n")
+        return sb.toString()
+    }
+}
+
+class Head(){
+    override fun toString(): String {
+        return "<head></head>"
+    }
 }
--- src/cn/song/ccBuilder/MyHtmlBuilderTest.kt
+++ src/cn/song/ccBuilder/MyHtmlBuilderTest.kt
@@ -3,10 +3,15 @@ package cn.song.ccBuilder
 /**
  * Created by hzsongzhengwang on 2015/9/11.
  */
+
+// because html() return a Html, so the result of result() is a Html object
 fun result() =
-        html{println("<html></html>")}
+        html{
+            head {
+            }
+        }
 
 
 fun main(args: Array<String>) {
-    result()
+    println(result())
 }
