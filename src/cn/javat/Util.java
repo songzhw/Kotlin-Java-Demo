a6b129985db88f36d610868ce5e03c3618006c71
songzhw <songzhw2012@gmail.com>
2016-01-06 17:36:45 +0800

    java security : Key , certs

---
+++ src/cn/javat/Util.java
@@ -0,0 +1,23 @@
+package cn.javat;
+
+/**
+ * Created by hzsongzhengwang on 2016/1/6.
+ */
+public class Util {
+
+    public static String bytesToHexString(byte[] data){
+        StringBuffer buf = new StringBuffer();
+        for (int i = 0; i < data.length; i++) {
+            int halfbyte = (data[i] >>> 4) & 0x0F;
+            int two_halfs = 0;
+            do {
+                if ((0 <= halfbyte) && (halfbyte <= 9))
+                    buf.append((char) ('0' + halfbyte));
+                else
+                    buf.append((char) ('A' + (halfbyte - 10)));
+                halfbyte = data[i] & 0x0F;
+            } while (two_halfs++ < 1);
+        }
+        return buf.toString();
+    }
+}
---
+++ src/cn/javat/sec/key/SynmmetricKeyTest.java
@@ -0,0 +1,40 @@
+package cn.javat.sec.key;
+
+
+import cn.javat.Base64;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.SecretKeySpec;
+
+/**
+ * Created by hzsongzhengwang on 2016/1/6.
+ */
+public class SynmmetricKeyTest {
+
+    public String createKey() throws Exception {
+        KeyGenerator keyGen = KeyGenerator.getInstance("DES");
+//        keyGen.init(56);  // java在非56时，会出错并提示“ Wrong keysize: must be equal to 56 ”
+        SecretKey secretKey = keyGen.generateKey();
+        byte[] key16 = secretKey.getEncoded();
+        String key16InBase64 = Base64.encodeToString(key16, Base64.DEFAULT);
+        System.out.println("szw createKey() : keyStr = "+key16InBase64);
+        return key16InBase64;
+    }
+
+    public void verifyKey(String keyStr) throws Exception {
+        byte[] key16 = Base64.decode(keyStr, Base64.DEFAULT);
+        System.out.println("222 len = "+key16.length);
+        SecretKeySpec keySpec = new SecretKeySpec(key16, "DES");
+        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
+        SecretKey key = keyFactory.generateSecret(keySpec);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SynmmetricKeyTest test = new SynmmetricKeyTest();
+        String keyStr = test.createKey();
+        test.verifyKey(keyStr);
+    }
+
+}
