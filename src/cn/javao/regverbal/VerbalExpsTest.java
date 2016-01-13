d623a73361cde8444cd0a70d5d976043c2095bc6
songzhw <songzhw2012@gmail.com>
2016-01-13 14:33:39 +0800

    learn https://github.com/VerbalExpressions/JavaVerbalExpressions

---
+++ src/cn/javao/regverbal/VerbalExpsTest.java
@@ -0,0 +1,38 @@
+package cn.javao.regverbal;
+
+/**
+ * Created by songzhw on 2016/1/13.
+ */
+public class VerbalExpsTest {
+    public static void main(String[] args) {
+        VerbalExpression testRegex = VerbalExpression.regex()
+                .startOfLine().then("http").maybe("s")
+                .then("://")
+                .maybe("www.").anythingBut(" ")
+                .endOfLine()
+                .build();
+
+        // Create an example URL
+        String url = "https://www.google.com";
+
+        // Use VerbalExpression's testExact() method to test if the entire string matches the regex
+        testRegex.testExact(url); //True
+
+        testRegex.toString(); // Outputs the regex used:
+        // ^(?:http)(?:s)?(?:\:\/\/)(?:www\.)?(?:[^\ ]*)$
+
+
+
+
+
+
+        String text = "aaabcd";
+        VerbalExpression regex = VerbalExpression.regex()
+                .find("a")
+                .capture().find("b").anything().endCapture().then("cd").build();
+
+        regex.getText(text);     // returns "abcd"
+        regex.getText(text, 1);  // returns "b"
+
+    }
+}
