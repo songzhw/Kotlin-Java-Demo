681d798f5bd1fa24f4948b8ff3cfa7df5133ff99
songzhw <songzhw2012@gmail.com>
2016-03-16 13:54:39 +0800

    Okio : ByteString

---
+++ src/javat/hongbao/HongBaoLeft.java
@@ -0,0 +1,15 @@
+package javat.hongbao;
+
+// 剩下的红包情况
+public class HongBaoLeft {
+    int    peoples;
+    double money;
+
+    @Override
+    public String toString() {
+        return "HongBaoLeft{" +
+                "peoples=" + peoples +
+                ", money=" + money +
+                '}';
+    }
+}
