d41bd7bdb0d04ea8a5538b7b9b26596cdc1c047e
songzhw <songzhw2012@gmail.com>
2016-03-18 20:53:12 +0800

    java8 lambda

--- Java8-Demo/src/lambda/Exercises02.java
+++ Java8-Demo/src/lambda/Exercises02.java
@@ -1,6 +1,12 @@
 package lambda;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 import java.util.concurrent.Callable;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 /**
  * Created by songzhw on 3/16/16.
@@ -9,6 +15,10 @@ public class Exercises02 {
     public static void main(String[] args) {
         Exercises02 obj = new Exercises02();
         obj.exer06();
+
+        obj.exer08();
+
+        obj.exer09();
 
     }
 
@@ -48,19 +58,51 @@ public class Exercises02 {
 
     // =======================================
 
-    // 7. Write a static method "andThen" that takes as parameters two "Runnable" instances
+    // 8. Write a static method "andThen" that takes as parameters two "Runnable" instances
     // and returns a "Runnable" that runs the first, then the second.
     // In the main method, pass two lambda expressions into a call to "andTehen",
     // and run the returned instance
-    public void exer07(){
+    public void exer08(){
+        String[] names = {"peter","paul","Mary"};
+        List<Runnable> runners = new ArrayList<>();
+        for(String name : names){
+            runners.add( ()-> System.out.println(name));
+        }
 
+         /*
+         * Error!!!
+         *     (Because "i" is not effectively final, so lambda report illeagl!)
+        for(int i = 0 ; i < names.length ; i++){
+            runners.add( ()-> System.out.println(names[i]));
+        }
+         * */
     }
 
-    public static void andThen(Runnable r1, Runnable r2){
 
+    // =======================================
+
+    // 9. Form a subclass "Collection2" from "Collection" and add a default method "void forEachIf(Consumer action, Predicater filter)
+    // t hat applies action to each element for which filter returns true. How could you use it?
+    public void exer09(){
+        Collection2<String> tmp = null; // just for example
+        tmp.forEachIf(System.out::println,
+                (String str) -> str.length() > 2);
     }
+
 }
 
 interface RunnableEx {
     void run() throws Exception;
+}
+
+
+
+interface Collection2<T> extends Collection<T> {
+    default void forEachIf(Consumer<T> action, Predicate<T> filter){
+        forEach( (T t) -> {
+            if(filter.test(t)){
+                action.accept(t);
+            }
+        });
+    }
 }
