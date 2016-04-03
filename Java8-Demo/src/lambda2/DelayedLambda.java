d73a511846733088d20114d547b871953bc09349
songzhw <songzhw2012@gmail.com>
2016-04-03 23:29:07 +0800

    java 8 lambda : a weird argument to the functionalInterface

--- Java8-Demo/src/lambda2/DelayedLambda.java
+++ Java8-Demo/src/lambda2/DelayedLambda.java
@@ -6,7 +6,7 @@ import javafx.scene.image.WritableImage;
 import javafx.scene.paint.Color;
 
 import java.util.function.Consumer;
-import java.util.function.Function;
+import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
 
 /**
@@ -19,7 +19,7 @@ public class DelayedLambda {
     public static void main(String[] args) {
         DelayedLambda obj = new DelayedLambda();
         obj.foo1();
-        obj.foo2();
+        obj.foo4();
 
     }
 
@@ -38,8 +38,10 @@ public class DelayedLambda {
     private void foo2(){
         Image img = new Image("res/kitty.jpg");
         Image brightImage = transform(img, Color::brighter);
+        // p.s. : public Color brighter()
     }
 
+    // UnaryOperator : public T apply(T)
     private Image transform(Image src, UnaryOperator<Color> func){
         int width = (int) src.getWidth();
         int height = (int) src.getHeight();
@@ -56,4 +58,66 @@ public class DelayedLambda {
 
         return ret;
     }
+
+    // 3. Compose
+    public Image foo3(){
+        Image img = new Image("res/kitty.jpg");
+        Image brightImage = transform(img, Color::brighter);
+        Image finalImage = transform(brightImage, Color::grayscale);
+        return finalImage;
+    }
+
+    // 3. An more efficient Compose (to reduce the temporary, hug Image in the memory)
+    public Image foo3_2(){
+        Image img = new Image("res/kitty.jpg");
+        Image ret = transform(img,
+                compose(Color::brighter, Color::grayscale));
+        return ret;
+    }
+
+    public <T>  UnaryOperator<T> compose(UnaryOperator<T> op1, UnaryOperator<T> op2) {
+        return t -> op2.apply( op1.apply(t) );
+    }
+
+    //4. confused about the foo2()
+    class Car{
+        public String name;
+        public Car(String name) {
+            this.name = name;
+        }
+        @Override
+        public String toString() {
+            return "Car{" + "name='" + name + '\'' + '}';
+        }
+
+        public Car polish(){
+            Car car = new Car("polished: " + this.name);
+            return car;
+        }
+
+        public Car transfer(Car old){
+            Car other = new Car("new car [from "+old.name+"]");
+            return other;
+        }
+
+    }
+
+    public void foo4(){
+        Car car = new Car("royal");
+        test4(car::polish, car::transfer);
+        test4(car::polish, Car::polish);
+    }
+
+    public void test4(Supplier<Car> supplier, UnaryOperator<Car> func){
+        // [Supplier] T get()
+        // [UnaryOperator] T apply(T t)
+
+        Car car2 = supplier.get();
+        System.out.println("foo4(1) : "+car2);       //=> foo4(1) : Car{name='polished: royal'}
+        Car newCar = func.apply(car2);
+        System.out.println("foo4(2) : "+newCar);     //=> foo4(2) : Car{name='new car [from polished: royal]'}
+    }
+
+
+
 }
