9a75d858d01494a533b31a65f8949f6a0f135af1
songzhw <songzhw2012@gmail.com>
2016-07-28 12:41:16 -0400

    thread : false wake

---
+++ Kotlin-Java-Demo/src/javat/thread/falsewake/FalseWakeStack.java
@@ -0,0 +1,31 @@
+package javat.thread.falsewake;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by songzhw on 2016-07-28.
+ *
+ * What's wrong with this code?
+ * : [Answer] false awake
+ */
+public class FalseWakeStack {
+    private List<String> list = new ArrayList<>();
+
+    public void push(String v){
+        synchronized (this) {
+            list.add(v);
+            notify();
+        }
+    }
+
+    public String pop() throws InterruptedException {
+        synchronized (this) {
+            if(list.size() <= 0) {
+                wait();
+            }
+            return list.remove(list.size()-1);
+        }
+    }
+
+}
