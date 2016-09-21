02772dde7c54a3b76f91d56890c0813f85ecc341
songzhw <songzhw2012@gmail.com>
2016-09-21 18:23:20 -0400

    Mockito : test a MVP demo

---
+++ JavaDemo/src/main/java/ca/six/demo/lib/mock/mvp/ILoadNoteCallback.java
@@ -0,0 +1,8 @@
+package ca.six.demo.lib.mock.mvp;
+
+/**
+ * Created by songzhw on 2016-09-21.
+ */
+public interface ILoadNoteCallback {
+    void onNotesLoaded(Note note);
+}
---
+++ JavaDemo/src/main/java/ca/six/demo/lib/mock/mvp/OneView.java
@@ -0,0 +1,18 @@
+package ca.six.demo.lib.mock.mvp;
+
+/**
+ * Created by songzhw on 2016-09-21.
+ */
+public class OneView {
+    public void showResult(boolean isSucc) {
+        if(isSucc){
+            // Toast.makeText().show();
+        }
+    }
+
+    public void setProgressBar(boolean b) {
+    }
+
+    public void openNoteDetails(int id) {
+    }
+}
