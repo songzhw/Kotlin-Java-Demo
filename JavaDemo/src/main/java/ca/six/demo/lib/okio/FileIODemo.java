1c654b2fcc5e45572fec0a95e19703f622dce470
songzhw <songzhw2012@gmail.com>
2017-01-03 21:06:28 -0500

    okio : write file

--- JavaDemo/src/main/java/ca/six/demo/lib/okio/FileIODemo.java
+++ JavaDemo/src/main/java/ca/six/demo/lib/okio/FileIODemo.java
@@ -1,5 +1,6 @@
 package ca.six.demo.lib.okio;
 
+import okio.BufferedSink;
 import okio.BufferedSource;
 import okio.Okio;
 
@@ -13,21 +14,37 @@ public class FileIODemo {
 
     public static void main(String[] args) {
         readFile();
+        write2File();
     }
 
-    public static void write2File(){
-
+    public static void write2File() {
+        // sink是输出相关的
+        try {
+            File file = new File("b"); // 位置得在工程根目录下！
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            BufferedSink sink = Okio.buffer(Okio.sink(file));
+            Charset utf8 = Charset.forName("utf-8");
+            sink.writeString("Ideal Community 2016.3.2", utf8);//不会换行的
+            sink.writeString("Android Studio 2.2\n", utf8);//换行了！
+            sink.writeString("test3", utf8);
+            sink.close();
+        } catch (Exception e) {
+            System.err.println("szw2 : " + e);
+        }
     }
 
-    public static void readFile(){
+    public static void readFile() {
+        // source是输入相关的
         try {
             File file = new File("a"); // 位置得在工程根目录下！
             BufferedSource bufferedSource = Okio.buffer(Okio.source(file));
             String str = bufferedSource.readByteString()
-                            .string(Charset.forName("utf-8"));
-            System.out.println("szw : "+str);
-        }catch (Exception e){
-            System.err.println("szw : "+e);
+                    .string(Charset.forName("utf-8"));
+            System.out.println("szw : " + str);
+        } catch (Exception e) {
+            System.err.println("szw : " + e);
         }
     }
 }
