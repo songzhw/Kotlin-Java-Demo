90808e39645293479c7da9b3a7359158b8a6e8bf
songzhw <songzhw2012@gmail.com>
2017-02-13 20:10:06 -0500

    singleton (thread safe)

--- JavaDemo/src/main/java/ca/six/demo/lib/thread/_volatile/Singleton.java
+++ JavaDemo/src/main/java/ca/six/demo/lib/thread/_volatile/Singleton.java
@@ -4,13 +4,14 @@ package ca.six.demo.lib.thread._volatile;
  * Created by songzhw on 2017-02-13.
  */
 public class Singleton {
+    // Singleton Instance is declared as volatile variable to ensure every thread see updated value for `instance`
     private volatile static Singleton instance;
 
     private Singleton(){}
 
     private Singleton getInstance(){
         if(instance == null){
-            synchronized (this) {
+            synchronized (Singleton.class) {
                 if(instance == null) {
                     instance = new Singleton();
                 }
