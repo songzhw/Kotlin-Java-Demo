37593c8edac865b67d3e0dbc869d0c0ca019880a
songzhw <songzhw2012@gmail.com>
2017-02-13 20:07:54 -0500

    singleton

---
+++ JavaDemo/src/main/java/ca/six/demo/lib/thread/_volatile/Singleton.java
@@ -0,0 +1,17 @@
+package ca.six.demo.lib.thread._volatile;
+
+/**
+ * Created by songzhw on 2017-02-13.
+ */
+public class Singleton {
+    private static Singleton instance;
+
+    private Singleton(){}
+
+    private Singleton getInstance(){
+        if(instance != null){
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
