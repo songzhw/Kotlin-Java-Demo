08dbc9e3287f6256253d7579eb4a9294104719c5
songzhw <songzhw2012@gmail.com>
2017-02-15 18:11:40 -0500

    juc semaphore

---
+++ JavaDemo/src/main/java/ca/six/demo/lib/thread/juc/SemaphoreTest.java
@@ -0,0 +1,52 @@
+package ca.six.demo.lib.thread.juc;
+
+import java.util.concurrent.Semaphore;
+
+public class SemaphoreTest {
+
+    Semaphore binary = new Semaphore(1);
+
+    public static void main(String args[]) {
+        final SemaphoreTest test = new SemaphoreTest();
+        // 起两个线程
+        new Thread() {
+            @Override
+            public void run() {
+                test.mutualExclusion();
+            }
+        }.start();
+
+        new Thread() {
+            @Override
+            public void run() {
+                test.mutualExclusion();
+            }
+        }.start();
+
+    }
+
+    private void mutualExclusion() {
+        try {
+            binary.acquire();
+
+            //mutual exclusive region
+            System.out.println(Thread.currentThread().getName() + " inside mutual exclusive region");
+            Thread.sleep(1000);
+
+        } catch (InterruptedException ie) {
+            ie.printStackTrace();
+        } finally {
+            binary.release();
+            System.out.println(Thread.currentThread().getName() + " outside of mutual exclusive region");
+        }
+    }
+
+}
+
+/*
+Output:
+    Thread-0inside mutual exclusive region
+    Thread-0outside of mutual exclusive region
+    Thread-1inside mutual exclusive region
+    Thread-1outside of mutual exclusive region
+*/
