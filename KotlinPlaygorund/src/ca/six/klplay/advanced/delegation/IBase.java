7f389828599b5464b88cb2c944109f576a2dbac7
songzhw <songzhw2012@gmail.com>
2017-06-23 00:19:42 -0400

    delegation

---
+++ KotlinPlaygorund/src/ca/six/klplay/advanced/delegation/IBase.java
@@ -0,0 +1,23 @@
+interface IBase {
+    fun print()
+    fun add(a: Int, b: Int): Int
+}
+
+class Base(val x: Int) : IBase {
+    override fun add(a: Int, b: Int): Int {
+        return a + b;
+    }
+
+    override fun print() {
+        print(x)
+    }
+}
+
+// only interfaces can be delegated to
+class Derived(b: IBase) : IBase by b
+
+fun main(args: Array<String>) {
+    val b = Base(30)
+    Derived(b).print() //=> 30    println(Derived(b).add(1, 2)) //=> 3
+}
+
