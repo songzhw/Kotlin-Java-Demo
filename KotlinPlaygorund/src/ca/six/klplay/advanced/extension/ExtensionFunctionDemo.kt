13706fc53f824d6f9868ade4660f1ace8fb89666
songzhw <songzhw2012@gmail.com>
2017-06-22 22:13:48 -0400

    what's the compiled version of kotlin file?

--- KotlinPlaygorund/src/ca/six/klplay/advanced/extension/ExtensionFunctionDemo.kt
+++ KotlinPlaygorund/src/ca/six/klplay/advanced/extension/ExtensionFunctionDemo.kt
@@ -8,6 +8,14 @@ fun main(args: Array<String>) {
     println("test".first(2))
 }
 
-
+/*
+What is this kotlin file after being compiled?
+```
+public final class ca.six.kdemo.advanced.extension.FunctionExtensionDemoKt {
+  public static final java.lang.String first(java.lang.String, int);
+  public static final void main(java.lang.String[]);
+}
+```
+*/
 
 
