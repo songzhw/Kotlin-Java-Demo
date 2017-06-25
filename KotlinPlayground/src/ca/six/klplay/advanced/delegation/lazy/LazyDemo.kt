18d355f44fedeb7d024ef8e2bb144332405c340c
songzhw <songzhw2012@gmail.com>
2017-06-25 10:05:59 -0400

    kotlin: lazy

--- KotlinPlayground/src/ca/six/klplay/advanced/delegation/lazy/LazyDemo.kt
+++ KotlinPlayground/src/ca/six/klplay/advanced/delegation/lazy/LazyDemo.kt
@@ -14,3 +14,20 @@ fun main(args: Array<String>) {
     println(lazyDemo.textView)
 }
 
+/*
+//=>
+    find view by id
+    textView
+    ===========
+    textView
+ */
+
+/*
+$ javap LazyDemo.class
+public final class LazyDemo extends Activity {
+  static final kotlin.reflect.KProperty[] $$delegatedProperties; //▼
+  public final java.lang.String getTextView();
+  public ca.six.kplay.delegation.LazyDemo();
+  static {}; //▼
+}
+ */
