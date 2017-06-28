95cc43bfedc2328d0737888b38ec13dbd510b949
songzhw <songzhw2012@gmail.com>
2017-06-27 20:32:03 -0400

    observe/veto

---
+++ KotlinPlayground/src/ca/six/klplay/advanced/delegation/VetoableDemo.kt
@@ -0,0 +1,18 @@
+package ca.six.klplay.advanced.delegation
+
+
+import kotlin.properties.Delegates
+
+class User2 {
+    var name: String by Delegates.vetoable("<no name>") { prop, old, new ->
+        println("$old -> $new")
+        false
+    }
+}
+
+fun main(args: Array<String>) {
+    val user = User2()
+    user.name = "first"
+    user.name = "second"
+    println(user.name)
+}
