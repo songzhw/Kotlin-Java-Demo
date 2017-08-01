f7ac836bbe6d0573c682a8695090e159729f9573
songzhw <songzhw2012@gmail.com>
2017-07-31 21:02:44 -0400

    reverse a linked list

---
+++ KotlinPlayground/src/ca/six/klplay/alg/ReverseLinkedListDemo.kt
@@ -0,0 +1,38 @@
+package ca.six.klplay.alg
+
+import kotlin.properties.Delegates
+
+class Node {
+    var num: Int  by Delegates.notNull<Int>()
+    var next: Node by Delegates.notNull<Node>()
+}
+
+class ReverseLinkedListDemo {
+    val end = Node()
+
+    fun reverse(head : Node) {
+        var first = head.next
+        var second = first.next
+        if (second == end) {
+            return
+        }
+        first.next = end // 前面second已经存了next.next
+
+        while(second.next != end){
+            var third = second.next
+
+            second.next = first
+            third.next = second
+            first = second
+            second = third
+
+        }
+        second.next = first
+        head.next = second
+    }
+
+}
+
+fun main(args: Array<String>) {
+    
+}
