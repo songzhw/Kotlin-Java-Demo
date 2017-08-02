ddf873583e62b9f123c14bcb6b01d3542e628380
songzhw <songzhw2012@gmail.com>
2017-08-02 11:13:39 -0400

    removed failed files

--- KotlinPlayground/src/ca/six/klplay/okhttp/download/DownloadDemo.kt
+++ KotlinPlayground/src/ca/six/klplay/okhttp/download/DownloadDemo.kt
@@ -1,7 +1,5 @@
 package ca.six.klplay.okhttp.download
 
-import ca.six.daily.core.network.download.ProgressListener
-import ca.six.daily.core.network.download.ProgressResponseBody
 import io.reactivex.Observable
 import io.reactivex.schedulers.Schedulers
 import okhttp3.*
@@ -11,33 +9,7 @@ import sun.security.krb5.Confounder.bytes
 
 
 fun downloadPic() {
-    val request = Request.Builder()
-            .url("https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2859174087,963187950&fm=23&gp=0.jpg")
-            .build()
 
-    val listener = object : ProgressListener {
-        override fun update(bytesRead: Long, contentLength: Long, isDone: Boolean) {
-            println("szw progress : $bytesRead , $contentLength , $isDone")
-            val percent: Int = (100 * bytesRead / contentLength).toInt()
-            println("szw progress = $percent")
-        }
-    }
-
-    val http = OkHttpClient.Builder().build()
-
-    Observable.create<Response> { emitter ->
-                val resp = http.newCall(request).execute()
-                emitter.onNext(resp)
-            }
-            .map { rawResp ->
-                ProgressResponseBody(rawResp.body()!!, listener)
-            }
-            .map { progressResp ->
-                progressResp.bytes()
-            }
-            .subscribe {
-                println("szw subscribe size = ${it.size}")
-            }
 
 }
 
--- KotlinPlayground/src/ca/six/klplay/okhttp/download/ProgressListener.kt
+++
@@ -1,5 +0,0 @@
-package ca.six.daily.core.network.download
-
-interface ProgressListener {
-    fun update(bytesRead : Long, contentLength : Long, isDone : Boolean)
-}
