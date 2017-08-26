168ce681634ae5a4f929b18b9fa969692d373df7
songzhw <songzhw2012@gmail.com>
2017-08-25 23:29:43 -0400

    inner class

--- JavaDemo/src/main/java/ca/six/demo/lib/innerclass/multiparents/Son.java
+++ JavaDemo/src/main/java/ca/six/demo/lib/innerclass/multiparents/Son.java
@@ -1,4 +1,22 @@
 package ca.six.demo.lib.innerclass.multiparents;
 
-public class Son extends Father, Mother {
+public class Son extends Father {
+    void foo(){
+        strength(); //=> from father
+
+        Son2 son2 = new Son2();
+        son2.inherit();
+    }
+
+    class Son2 extends Mother {
+        void inherit(){
+            calm();
+        }
+    }
+
+    public static void main(String[] args) {
+        Son son = new Son();
+        son.foo();
+    }
 }
+
