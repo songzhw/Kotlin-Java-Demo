0158736c00d4b15c3b491d5de64f87a70888f908
songzhw <songzhw2012@gmail.com>
2017-08-26 20:33:33 -0400

    fix inner class error

--- JavaDemo/src/main/java/ca/six/demo/lib/innerclass/multiparents/Son.java
+++ JavaDemo/src/main/java/ca/six/demo/lib/innerclass/multiparents/Son.java
@@ -3,8 +3,8 @@ package ca.six.demo.lib.innerclass.multiparents;
 public class Son extends Father {
     private int age = 10;
 
-    class Son2 extends Mother {
-        void inherit(){
+    public class Son2 extends Mother {
+        public void inherit(){
             System.out.println("Inner class : "+age); //=> Inner class : 10
         }
     }
