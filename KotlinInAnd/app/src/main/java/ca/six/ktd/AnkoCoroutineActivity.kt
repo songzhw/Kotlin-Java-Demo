571aefcaccc8a86ec288b604c9a419f22377ee20
songzhw <songzhw2012@gmail.com>
2017-10-06 22:26:43 -0400

    Main Thread got blocked

--- KotlinInAnd/app/src/main/java/ca/six/ktd/AnkoCoroutineActivity.kt
+++ KotlinInAnd/app/src/main/java/ca/six/ktd/AnkoCoroutineActivity.kt
@@ -2,11 +2,27 @@ package ca.six.ktd
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_anko_coroutine.*
 
-class AnkoCoroutineActivity : Activity() {
+class AnkoCoroutineActivity : Activity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_anko_coroutine)
+
+        btnCoroutineDemo.setOnClickListener(this)
+        getData()
+    }
+
+    override fun onClick(p0: View?) {
+        println("szw click")
+    }
+
+    fun getData() : Int{
+        println("szw Thread : "+Thread.currentThread().name)
+        Thread.sleep(5000)
+        println("szw sleep over")
+        return 23
     }
 }
