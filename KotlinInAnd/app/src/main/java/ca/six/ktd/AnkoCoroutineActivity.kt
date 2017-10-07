7542bea2f79b0813c43315c7539aa5143e280202
songzhw <songzhw2012@gmail.com>
2017-10-06 21:57:52 -0400

    init repo

---
+++ KotlinInAnd/app/src/main/java/ca/six/ktd/AnkoCoroutineActivity.kt
@@ -0,0 +1,12 @@
+package ca.six.ktd
+
+import android.app.Activity
+import android.os.Bundle
+
+class AnkoCoroutineActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_anko_coroutine)
+    }
+}
