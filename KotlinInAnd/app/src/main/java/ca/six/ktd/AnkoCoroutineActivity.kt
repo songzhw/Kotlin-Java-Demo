3d5c5591a0e444af411b60e01c7ec5045cde69c9
songzhw <songzhw2012@gmail.com>
2017-10-06 22:46:12 -0400

    kotlin anko coroutine

--- KotlinInAnd/app/src/main/java/ca/six/ktd/AnkoCoroutineActivity.kt
+++ KotlinInAnd/app/src/main/java/ca/six/ktd/AnkoCoroutineActivity.kt
@@ -4,6 +4,9 @@ import android.app.Activity
 import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_anko_coroutine.*
+import kotlinx.coroutines.experimental.Deferred
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.async
 import org.jetbrains.anko.coroutines.experimental.bg
 
 class AnkoCoroutineActivity : Activity(), View.OnClickListener {
@@ -13,8 +16,12 @@ class AnkoCoroutineActivity : Activity(), View.OnClickListener {
         setContentView(R.layout.activity_anko_coroutine)
 
         btnCoroutineDemo.setOnClickListener(this)
-        bg {
-            getData()
+
+        async(UI){
+            val value : Deferred<Int> = bg {
+                getData()
+            }
+            showData( value.await())
         }
     }
 
@@ -22,10 +29,15 @@ class AnkoCoroutineActivity : Activity(), View.OnClickListener {
         println("szw click")
     }
 
-    fun getData() : Int{
-        println("szw Thread : "+Thread.currentThread().name)
+    fun getData(): Int {
+        println("szw Thread1 : " + Thread.currentThread().name)
         Thread.sleep(5000)
         println("szw sleep over")
         return 23
+    }
+
+    fun showData(value: Int) {
+        println("szw Thread2 : " + Thread.currentThread().name)
+        btnCoroutineDemo.setText("new " + value)
     }
 }
