c358ef9a3058bf08d37ffafc218c7a9cbd03ad08
songzhw <songzhw2012@gmail.com>
2017-10-06 21:09:57 -0400

    kotlin coroutine

--- KotlinPlayground/src/main/kotlin/ca/six/klplay/advanced/coroute/CoroutineDemo01.kt
+++ KotlinPlayground/src/main/kotlin/ca/six/klplay/advanced/coroute/CoroutineDemo01.kt
@@ -5,7 +5,7 @@ import kotlinx.coroutines.experimental.delay
 import kotlinx.coroutines.experimental.launch
 import kotlinx.coroutines.experimental.runBlocking
 
-fun foo(){
+fun foo() {
     launch(CommonPool) {
         delay(1000L)
         println("World1")
@@ -15,7 +15,7 @@ fun foo(){
     Thread.sleep(2000L)
 }//=> Hello, 1秒后world. 再1秒后结束应用
 
-fun fo2(){
+fun fo2() {
     launch(CommonPool) {
         delay(1000L)
         println("World2")
@@ -50,16 +50,40 @@ fun fo4() = runBlocking<Unit> {
 
 // join(): 等待协程的工作完
 fun fo5() = runBlocking<Unit> {
-    val job = launch(CommonPool){
+    val job = launch(CommonPool) {
         delay(1000)
-        println ("World5")
+        println("World5")
     }
     print("Hello ")
     job.join()
 }
 
+// without "suspend", we will get a error:
+//    "delay" should be called only from a coroutine or another suspended function
+fun fo6(myFun: () -> Unit) = runBlocking<Unit> {
+    var job = launch(CommonPool) {
+        delay(1000)
+        myFun()
+    }
+
+    print("Hello ")
+    job.join()
+}
+
+
+fun world6(){
+    println("World6")
+}
+
 fun main(args: Array<String>) {
-    fo5()
+//    fo6(world6()) //=> This is just like "fo6(Unit)"
+    // 传递函数, 下面二方法都行
+    fo6({ world6()})
+    fo6 { world6() }
+    /*
+    fo6 {world6()} 的结果, 是第一个World6和第二个Hello一起出现
+    因为我们前面在join()了嘛, 所以没有两个Hellow一起出来!
+     */
 }
 
 // https://baiyangcao.github.io/notes/2017/06/20/kotlin-coroutines.html
