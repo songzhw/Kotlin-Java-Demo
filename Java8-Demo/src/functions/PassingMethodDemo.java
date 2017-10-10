b70a41e66e73aac8c71c9ba7d1a443a54991fc11
songzhw <songzhw2012@gmail.com>
2017-10-10 19:34:09 -0400

    pass function

--- Java8-Demo/src/functions/PassingMethodDemo.java
+++ Java8-Demo/src/functions/PassingMethodDemo.java
@@ -30,6 +30,32 @@ public class PassingMethodDemo {
 
     }
 
+    public void work(){
+        Grade grade = getGrade();
+        grade.id = 100;
+        List<User> students = getUser;
+
+        for(User user : students){
+            user.id += 10;
+        }
+
+        for(User user: students){
+            System.out.println("szw user = "+user);
+        }
+    }
+
+    public List<User> getAllStudents(Grade grade){
+        return grade.students;
+    }
+
+    public List<User> getTop3Students(Grade grade){
+        List<User> user = grade.students.subList(0, 2);
+        user.add(new User(0, "inserted"));
+        return user;
+    }
+
+
+
     public static void main(String[] args) {
        PassingMethodDemo obj = new PassingMethodDemo();
        obj.workAll();
