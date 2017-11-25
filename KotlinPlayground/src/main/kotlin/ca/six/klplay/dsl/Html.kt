063ffdffc4c3bbfc7cf49273f08bf03dfb03348e
songzhw <songzhw2012@gmail.com>
2017-11-25 10:15:15 -0500

    kotlin dsl

--- KotlinPlayground/src/main/kotlin/ca/six/klplay/dsl/Html.kt
+++ KotlinPlayground/src/main/kotlin/ca/six/klplay/dsl/Html.kt
@@ -3,13 +3,9 @@ package ca.six.klplay.dsl
 class Html {
     val children = arrayListOf<Tag>()
 
-    fun head(work: Head.()->Unit) : Head {
-        return childWork(Head(), work)
-    }
+    fun head(work: Head.()->Unit) : Head  = childWork(Head(), work)
 
-    fun body(work: Body.()->Unit) : Body {
-        return childWork(Body(), work)
-    }
+    fun body(work: Body.()->Unit) : Body =  childWork(Body(), work)
 
     fun <T : Tag> childWork(obj : T, work : T.()->Unit) : T {
         obj.work()
