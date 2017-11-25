1ee6823c6c4515be0ff63259b7d9b93d492c3fa0
songzhw <songzhw2012@gmail.com>
2017-11-25 10:15:02 -0500

    kotlin dsl

--- KotlinPlayground/src/main/kotlin/ca/six/klplay/dsl/Html.kt
+++ KotlinPlayground/src/main/kotlin/ca/six/klplay/dsl/Html.kt
@@ -4,17 +4,17 @@ class Html {
     val children = arrayListOf<Tag>()
 
     fun head(work: Head.()->Unit) : Head {
-        val head = Head()
-        head.work()
-        children.add(head)
-        return head
+        return childWork(Head(), work)
     }
 
     fun body(work: Body.()->Unit) : Body {
-        val body = Body()
-        body.work()
-        children.add(body)
-        return body
+        return childWork(Body(), work)
     }
 
+    fun <T : Tag> childWork(obj : T, work : T.()->Unit) : T {
+        obj.work()
+        children.add(obj)
+        return obj
+    }
 }
+
