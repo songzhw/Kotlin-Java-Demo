71d0756e25a1f38d6ec3a962d5c26218a7ebff97
songzhw <songzhw2012@gmail.com>
2017-11-26 01:04:35 -0500

    kotlin dsl

--- KotlinPlayground/src/main/kotlin/ca/six/klplay/dsl/Tag.kt
+++ KotlinPlayground/src/main/kotlin/ca/six/klplay/dsl/Tag.kt
@@ -1,12 +1,26 @@
 package ca.six.klplay.dsl
 
-open class Tag {
-    fun self() {}
+open class Tag() {
+    val children = arrayListOf<Tag>()
+    val sb = StringBuilder()
+
+    fun <T : Tag> childWork(obj : T, work : T.()->Unit) : T {
+        obj.work()
+        children.add(obj)
+        return obj
+    }
+
+    operator fun String.unaryPlus() {
+        sb.append(this) // 这个this可是指String @
+        sb.append("\n")
+    }
 }
 
 class Body : Tag() {
 }
 
 class Head : Tag() {
-
+    fun title(block : () -> Unit){
+        block()
+    }
 }
