c148f6e275d902c8e5e34db6a2c02957076fdc7d
songzhw <songzhw2012@gmail.com>
2018-02-28 15:28:26 -0500

    groovy DSL

--- GroovyDemo/src/main/groovy/DslDemo.groovy
+++ GroovyDemo/src/main/groovy/DslDemo.groovy
@@ -44,7 +44,16 @@ first 23 action {
 // =============================================
 
 def fun1(a){}
-
 fun1 13  //只要不是无参, 方法调用时就可以省略()
 
 
+def apply(Map<String, String> args) {
+    args.forEach { key, value ->
+        println("szw $key $value")
+    }
+}
+
+//=> szw plugin groovy
+apply([plugin: 'java'])
+// apply [plugin: 'java']  // 这种不行, 会出错. 可能是在省了()后只用[]有歧义
+apply plugin: "groovy"   // 若key是普通字符串, 还可以省略掉绰号
