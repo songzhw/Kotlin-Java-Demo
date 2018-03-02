858e3965c950b4ecec6a100284e6b35efdd6438a
songzhw <songzhw2012@gmail.com>
2018-03-02 12:34:28 -0500

    completable API

--- Java8-Demo/test/cinoketabke/CompletableDemoTest.java
+++ Java8-Demo/test/cinoketabke/CompletableDemoTest.java
@@ -1,5 +1,7 @@
 package cinoketabke;
 
+import com.sun.xml.internal.ws.util.CompletedFuture;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,10 +12,38 @@ public class CompletableDemoTest {
 
     // getNow() 返回完成后的结果, 在这就是"first". 若还没完成就返回传入的null(默认值)
     @Test
-    public void testCopmleted(){
+    public void testCopmleted() {
         // 这是新建一个已完成的任务(completed), 所以下面的isDone()是true
         CompletableFuture future = CompletableFuture.completedFuture("first");
         assertTrue(future.isDone());
         assertEquals("first", future.getNow(null));
+    }
+
+    @Test
+    public void testAsync() {
+        CompletableFuture future = CompletableFuture.runAsync(() -> {
+            System.out.println("testAsync() : 01 Thread = " + Thread.currentThread().getName());
+            SystemClock.sleep(200);
+        });
+
+        System.out.println("testAsync() : 02 Thread = " + Thread.currentThread().getName());
+        assertFalse((future.isDone()));
+        SystemClock.sleep(340);
+        assertTrue(future.isDone());
+    }
+    /* 注意执行顺序
+        testAsync() : 02 Thread = main
+        testAsync() : 01 Thread = ForkJoinPool.commonPool-worker-1
+     */
+
+}
+
+class SystemClock {
+    static void sleep(long millsecond) {
+        try {
+            Thread.sleep(millsecond);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
