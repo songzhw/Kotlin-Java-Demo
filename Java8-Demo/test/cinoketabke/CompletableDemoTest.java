6712f190b1c6c9e24521e3306c1eee24fcf9b667
songzhw <songzhw2012@gmail.com>
2018-03-02 12:35:06 -0500

    completable API

--- Java8-Demo/test/cinoketabke/CompletableDemoTest.java
+++ Java8-Demo/test/cinoketabke/CompletableDemoTest.java
@@ -38,23 +38,47 @@ public class CompletableDemoTest {
 
 
     // then是指上一阶段(即完成后得到msg), apply是用上一阶段的结果来接着做下一阶段的事
-@Test
-public void testStages() {
-    CompletableFuture future =
-        CompletableFuture.completedFuture("msg")
-            .thenApply(msg -> {
-                System.out.println("testStages() : 01 Thread = " + Thread.currentThread().getName());
-                return msg.toUpperCase();
-            });  //.thenApply(String::toUpperCase)也行
+    @Test
+    public void testStages() {
+        CompletableFuture future =
+            CompletableFuture.completedFuture("msg")
+                .thenApply(msg -> {
+                    System.out.println("testStages() : 01 Thread = " + Thread.currentThread().getName());
+                    return msg.toUpperCase();
+                });  //.thenApply(String::toUpperCase)也行
 
-    System.out.println("testStages() : 02 Thread = " + Thread.currentThread().getName());
-    assertEquals("MSG", future.getNow(null));
-}
-/*
-    因为都在main线程中, 所以等执行到getNow(), 这个future其实已经完成了
-    testStages() : 01 Thread = main
-    testStages() : 02 Thread = main
-*/
+        System.out.println("testStages() : 02 Thread = " + Thread.currentThread().getName());
+        assertEquals("MSG", future.getNow(null));
+    }
+    /*
+        因为都在main线程中, 所以等执行到getNow(), 这个future其实已经完成了
+        testStages() : 01 Thread = main
+        testStages() : 02 Thread = main
+    */
+
+
+
+
+    @Test
+    public void testStagesAsync() {
+        CompletableFuture future =
+            CompletableFuture.completedFuture("msg")
+                .thenApplyAsync(msg -> {
+                    System.out.println("testStagesAsync() : 01 Thread = " + Thread.currentThread().getName());
+                    SystemClock.sleep(200);
+                    return msg.toUpperCase();
+                });  //.thenApply(String::toUpperCase)也行
+
+        System.out.println("testStagesAsync() : 02 Thread = " + Thread.currentThread().getName());
+        assertNull(future.getNow(null)); //为null, 是因为这时async还没有完成嘛.
+
+        assertEquals("MSG", future.join()); //类似Thread.join(), 等你完成, 这时能得到结果
+    }
+
+    /* 注意执行顺序
+        testStagesAsync() : 02 Thread = main
+        testStagesAsync() : 01 Thread = ForkJoinPool.commonPool-worker-1
+    */
 
 
 
