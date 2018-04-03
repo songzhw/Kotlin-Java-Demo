c0f85bc5e9786e638b55116b0eea5a6c7ca0d554
songzhw <songzhw2012@gmail.com>
2018-04-03 10:39:45 -0400

    stack without duplication

--- JavaDemo2/src/main/java/ca/six/demo/collections/stack/UniqueStack.java
+++ JavaDemo2/src/main/java/ca/six/demo/collections/stack/UniqueStack.java
@@ -1,22 +1,50 @@
 package ca.six.demo.collections.stack;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 // java.util.Stack does not have the contains(object), remove(object) methods. So we can not use java.util.Stack directly.
 // so we are going to use Deque to implement a stack without duplicated items
 public class UniqueStack<E> {
+    private Deque<E> deque = new ArrayDeque<>(); // LinkedList也是Deque的一个实现类
 
     public void push(E item) {
-
+        boolean isContaining = deque.contains(item);
+        if (isContaining) {
+            deque.remove(item);
+        }
+        deque.addFirst(item);
     }
 
-    public E pop(){
-        return null;
+    public E pop() {
+        return deque.getFirst();
     }
 
-    public E peek(){
-        return null;
+    public E peek() {
+        return deque.peekFirst();
     }
 
-    public boolean isEmpty(){
-        return false;
+    public boolean isEmpty() {
+        return deque.isEmpty();
+    }
+
+    void print() {
+        for (E e : deque) {
+            System.out.println("szw e = " + e);
+        }
+    }
+}
+
+class Demo {
+    public static void main(String[] args) {
+        UniqueStack<Integer> stack = new UniqueStack<>();
+        stack.push(23);
+        stack.push(3);
+        stack.push(4);
+        stack.push(23);
+        stack.push(4);
+        stack.push(2);
+
+        stack.print(); //=> 从顶到底是 2, 4, 23, 3
     }
 }
