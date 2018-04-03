7d6c872ee067c7563f5a1113b78ab8fa9a78b08f
songzhw <songzhw2012@gmail.com>
2018-04-03 10:29:08 -0400

    stack without duplication

---
+++ JavaDemo2/src/main/java/ca/six/demo/collections/stack/UniqueStack.java
@@ -0,0 +1,22 @@
+package ca.six.demo.collections.stack;
+
+// java.util.Stack does not have the contains(object), remove(object) methods. So we can not use java.util.Stack directly.
+// so we are going to use Deque to implement a stack without duplicated items
+public class UniqueStack<E> {
+
+    public void push(E item) {
+
+    }
+
+    public E pop(){
+        return null;
+    }
+
+    public E peek(){
+        return null;
+    }
+
+    public boolean isEmpty(){
+        return false;
+    }
+}
