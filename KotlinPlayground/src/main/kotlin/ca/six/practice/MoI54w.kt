2af5c33743b10235e39ad65da2296624cbda9cdc
songzhw <songzhw2012@gmail.com>
2018-05-17 22:22:08 -0400

    jump out of loops

--- KotlinPlayground/src/main/kotlin/ca/six/practice/MoI54w.kt
+++ KotlinPlayground/src/main/kotlin/ca/six/practice/MoI54w.kt
@@ -2,7 +2,7 @@ package ca.six.practice
 
 import java.net.URL
 
-fun urlAndString(){
+fun urlAndString() {
     val url = URL("https://github.com/abc/a/issues/12")
     println("host = ${url.host}, path = ${url.path}")  //=> host = github.com, path = /abc/a/issues/12
 
@@ -11,6 +11,27 @@ fun urlAndString(){
     querys.forEach { println(it) }
 
 }
+
+fun jumpOutofOneLoop() {
+    // 跳出内层循环. 3-4后是4-1, 打到4-4, 接着是5-1, ..., 一直到10-3, 10-4
+    for (i in 1..10) {
+        for (j in 1..10) {
+            println("$i, $j")
+            if (i > 2 && j > 3) break
+        }
+    }
+}
+
+fun jumpOutOfTwoLoops() {
+    //打印完1,2后, 打印3-1, 3-2, 3-3, 3-4, 就完了
+    loop@ for (i in 1..10) {
+        for (j in 1..10) {
+            println("$i, $j")
+            if (i > 2 && j > 3) break@loop
+        }
+    }
+}
+
 
 fun main(args: Array<String>) {
     urlAndString()
