9128f3321239654a189828f8fe11cce4b8d1017d
songzhw <songzhw2012@gmail.com>
2018-05-18 01:16:40 -0400

    mo i54w

--- KotlinPlayground/src/main/kotlin/Temp.kt
+++ KotlinPlayground/src/main/kotlin/Temp.kt
@@ -1,26 +1,4 @@
-import java.net.URL
-
-data class A(val id: Int, val time: Long) {
-}
 
 fun main(args: Array<String>) {
-    val list: List<A> = arrayListOf<A>(A(11, 20), A(23, 20), A(11, 23), A(17, 24))
-    list
-            .sortedWith(Comparator<A> { a1, a2 ->
-                if (a1.id == a2.id) {
-                    a2.time.compareTo(a1.time)
-                } else {
-                    a2.id.compareTo(a1.id)
-                }
-            })
-//            .take(3)
-            .forEach { A ->
-                println("$A")
-            }
-/*
-Comparator<A> { a1, a2 ->
-                a2.id.compareTo(a1.id)
-            }
- */
 
 }
---
+++ KotlinPlayground/src/main/kotlin/ca/six/practice/MoI54w_2.kt
@@ -0,0 +1,73 @@
+package ca.six.practice
+
+import java.net.URL
+import java.util.*
+
+
+class SearchResult(val url: String, val score: Int, val timeStamp: Long) {
+}
+
+interface SearchModel {
+    fun load(url: String, timestamp: Date, isOpenTab: Boolean)
+    fun search(query: String): List<SearchResult>
+}
+
+class UrlItem(val url: String, val host: String, val path: String, val timeStamp: Long, val isOpenTab: Boolean) {
+}
+
+// Mock a database
+//val database: HashMap<String, UrlItem> = HashMap()
+val database: ArrayList<UrlItem> = ArrayList()
+
+class SimpleSearchModel : SearchModel {
+    override fun load(url: String, timestamp: Date, isOpenTab: Boolean) {
+        val urlObject = URL(url)
+        database.add(UrlItem(url, urlObject.host, urlObject.path, timestamp.time, isOpenTab))
+    }
+
+    override fun search(query: String): List<SearchResult> {
+        val searchResults: ArrayList<SearchResult> = ArrayList()
+
+        val keywords = query.split(" ")
+        database.forEach nextUrlItem@ { urlItem ->
+            var score = 0
+            keywords.forEach { keyword ->
+                var scoreInThisKeyword = 0
+                val url = urlItem.url
+
+                // calculate the score
+                if (keyword.equals(url)) {
+                    scoreInThisKeyword += 50
+                } else if (url.contains(keyword)) {
+                    scoreInThisKeyword += 25
+                }
+
+                if (urlItem.path.contains(keyword)) {
+                    scoreInThisKeyword += 10
+                }
+
+                // if no match, move on to next urlItem;
+                if (scoreInThisKeyword == 0) {
+                    return@nextUrlItem
+                } else {
+                    score += scoreInThisKeyword
+                }
+            }
+
+
+            if (urlItem.isOpenTab) {
+                score += 250
+            }
+
+
+            // every query item is matched, then add it to the searchResult
+            val result = SearchResult(urlItem.url, score, urlItem.timeStamp)
+            searchResults.add(result)
+        }
+
+
+
+        return searchResults
+    }
+
+}
