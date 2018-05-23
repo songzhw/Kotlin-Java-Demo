863a36bc9b90d8a1a1b2d5814685bb197fa1ab73
songzhw <songzhw2012@gmail.com>
2018-05-23 14:19:10 -0400

    java/kotlin generics: reified

--- KotlinPlayground/src/main/kotlin/ca/six/kex/generics/ReifiedDemo.kt
+++ KotlinPlayground/src/main/kotlin/ca/six/kex/generics/ReifiedDemo.kt
@@ -2,10 +2,13 @@ package ca.six.kex.generics
 
 // 普通的泛型
 fun <T> fromJson(json: String, clz: Class<T>) {
+//    println("class01 = ${T::class.java}")  // Error!
 }
 
 // 带reified的泛型 (间接帮助我们拿到了T.class)
 inline fun <reified T> fromJson(json: String){
+    val clz = T::class.java
+    println("class = $clz")   //=> class = class ca.six.kex.generics.Person
     fromJson(json, T::class.java)
 }
 
@@ -13,4 +16,8 @@ class Person(var id: Int, var name: String)
 
 fun test(){
     fromJson<Person>("""{"id": 0, "name": "Jack" }""")
+}
+
+fun main(args: Array<String>) {
+    test()
 }
