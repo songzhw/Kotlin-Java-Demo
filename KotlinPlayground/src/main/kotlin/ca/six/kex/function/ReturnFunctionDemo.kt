08fe38c31623540e64bcdb6375a7fa56af46410e
songzhw <songzhw2012@gmail.com>
2018-05-30 09:30:55 -0400

    kotlin return function; local variable in function?!

---
+++ KotlinPlayground/src/main/kotlin/ca/six/kex/function/ReturnFunctionDemo.kt
@@ -0,0 +1,22 @@
+package ca.six.kex.function
+
+import kotlin.reflect.KFunction0
+
+fun foo(): KFunction0<Unit> {
+    var a = 1
+    fun inner(){
+        a++
+        println("a = $a")
+    }
+    return ::inner
+}
+
+fun main(args: Array<String>) {
+    var func = foo()
+    func() //=> 2
+    func() //=> 3
+    func() //=> 4
+
+    foo()
+    foo()
+}
