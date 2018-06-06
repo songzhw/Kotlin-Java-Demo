b6a35e3d7daa7ac9d4f39dffba85547b7a2f439d
songzhw <songzhw2012@gmail.com>
2018-06-06 17:11:34 -0400

    dynamic proxy

--- AdvancedJ/src/main/java/Temp.java
+++ AdvancedJ/src/main/java/Temp.java
@@ -1,26 +1,52 @@
-import java.util.function.Supplier;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+interface IClickListener {
+    void say(String s);
+
+}
+
+class ButtonClickListener implements IClickListener {
+    @Override
+    public void say(String s) {
+        System.out.println("hello " + s);
+    }
+
+}
+
+class TraceHelper implements InvocationHandler {
+    private IClickListener target;  // 原始对象
+
+    public TraceHelper(IClickListener target) {
+        this.target = target;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        System.out.println("before print");
+        method.invoke(target, args);  // 调用原始对象的方法
+        System.out.println("after print");
+        return null;
+    }
+
+}
 
 public class Temp {
-
-    private String looper;
-    private String callback;
-
-    public Temp(String looper) {
-        this(looper, null);
+    public static void main(String[] args) {
+        IClickListener hello = enhanceHello(new ButtonClickListener());
+        hello.say("world");
     }
 
-    public Temp() {
-        this("", null);
-    }
-
-    public Temp(String looper, String callback) {
-        this.looper = looper;
-        this.callback = callback;
-    }
-
-    public void sendMessage(){
-
+    public static IClickListener enhanceHello(IClickListener target) {
+        return (IClickListener) Proxy.newProxyInstance(
+                Temp.class.getClassLoader(),
+                new Class<?>[]{IClickListener.class},
+                new TraceHelper(target)
+        );
     }
 }
 
-class User{}
+
+
+
