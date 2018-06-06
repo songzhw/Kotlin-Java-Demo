11303fd01a859f93c91ef6b7f842fdb6d6e933e2
songzhw <songzhw2012@gmail.com>
2018-06-06 10:01:54 -0400

    其它线程中启handler, 立即结束了

--- AdvancedJ/src/main/kotlin/msg_queue/demo/HandlerDemo.kt
+++ AdvancedJ/src/main/kotlin/msg_queue/demo/HandlerDemo.kt
@@ -1,14 +1,10 @@
 package msg_queue.demo
 
 import msg_queue.*
+import kotlin.concurrent.thread
 
-class HandlerDemo {
-
-}
-
-// 没效果. 可能是looper没有启动
-fun main(args: Array<String>) {
-    val handler = object: Handler() {
+fun main1(args: Array<String>) {
+    val handler = object : Handler() {
         override fun handleMessage(msg: Message) {
             println("szw handler handleMesage(${msg.what})")
         }
@@ -28,4 +24,26 @@ fun main(args: Array<String>) {
     Looper.loop()
     println("02")  //=> 这一句永远就不执行了
 
+}
+
+fun main(args: Array<String>) {
+    // 不加isDaemon=true, 就报错 "Exception in thread "main" java.lang.IllegalThreadStateException"
+    thread(start = true, isDaemon = true) {
+        val handler = object : Handler() {
+            override fun handleMessage(msg: Message) {
+                println("szw handler handleMesage(${msg.what})")
+            }
+        }
+
+        val msg = Message()
+        msg.what = 11
+        msg.obj = "test" //要是obj是Object类型, 这就会报错. 正确得是Any类型
+
+        handler.sendMessage(msg)
+
+        println("011")
+        Looper.loop()
+        println("022")
+
+    }
 }
