02aee9030523532c88542f0861010a69a412af66
songzhw <songzhw2012@gmail.com>
2018-06-07 09:42:41 -0400

    dynamic proxy + vararg

--- AdvancedJ/src/main/kotlin/aop/proxy/ProxyDemo.kt
+++ AdvancedJ/src/main/kotlin/aop/proxy/ProxyDemo.kt
@@ -5,79 +5,84 @@ import java.lang.reflect.Method
 import java.lang.reflect.Proxy
 
 
-//interface IClickListener {
-//    fun onClick()
-//}
-//
-//class ButtonClickListener : IClickListener {
-//    override fun onClick() {
-//        println("szw click button")
-//    }
-//}
-//
-//class TraceHelper(val listener: IClickListener) : InvocationHandler {
-//    override fun invoke(proxy: Any?, method: Method?, args: Array<out Any>): Any {
-//        println("szw before click")
-//        method?.invoke(listener, args)
-//        println("szw trace.track click event")
-//        return Unit
-//    }
-//}
-//
-//
-//fun main(args: Array<String>) {
-//    val listener : IClickListener = ButtonClickListener()
-//    val tracedLIstener = TraceHelper(listener)
-//
-//    val classLoader = IClickListener::class.java.classLoader
-//    val proxyed : IClickListener =
-//            Proxy.newProxyInstance(
-//                    classLoader,
-//                    arrayOf<Class<*>>(IClickListener::class.java),
-//                    tracedLIstener
-//            ) as IClickListener
-//
-//    proxyed.onClick()
-//}
-
-
-
 interface IClickListener {
-    fun say(s: String)
+    fun onClick()
 }
 
 class ButtonClickListener : IClickListener {
-    override fun say(s: String) {
-        println("hello $s")
+    override fun onClick() {
+        println("szw click button")
     }
-
 }
 
-class TraceHelper(private val target: IClickListener  // 原始对象
-) : InvocationHandler {
-
-    @Throws(Throwable::class)
-    override fun invoke(proxy: Any, method: Method, args: Array<Any>): Any? {
-        println("before print")
-        method.invoke(target, args)  // 不加*, 就会有错: IllegalArgumentException: argument type mismatch
-        println("after print")
-        return null
+class TraceHelper(val listener: IClickListener) : InvocationHandler {
+    override fun invoke(proxy: Any?, method: Method?, args: Array<out Any>?): Any {
+        println("szw before click")
+        println("$args, ${args?.size}") //=> onClick()这样没有入参的情况下, args是null. 所以反射时要注意, 得传一个空Array进去才行
+        if(args == null){
+            method?.invoke(listener, *arrayOf()) //arrayOf()返回一个array. 要放入vararg中, 就得加个spread operator
+        } else {
+            method?.invoke(listener, *args)
+        }
+        println("szw trace.track click event")
+        return Unit
     }
-
 }
+
 
 fun main(args: Array<String>) {
-    val hello = enhanceHello(ButtonClickListener())
-    hello.say("world")
+    val listener : IClickListener = ButtonClickListener()
+    val tracedLIstener = TraceHelper(listener)
+
+    val classLoader = IClickListener::class.java.classLoader
+    val proxyed : IClickListener =
+            Proxy.newProxyInstance(
+                    classLoader,
+                    arrayOf<Class<*>>(IClickListener::class.java),
+                    tracedLIstener
+            ) as IClickListener
+
+    proxyed.onClick()
 }
 
-fun enhanceHello(target: IClickListener): IClickListener {
-    return Proxy.newProxyInstance(
-            IClickListener::class.java.classLoader,
-            arrayOf<Class<*>>(IClickListener::class.java),
-            TraceHelper(target)
-    ) as IClickListener
-}
+
+//
+//interface IClickListener {
+//    fun say(s: String)
+//}
+//
+//class ButtonClickListener : IClickListener {
+//    override fun say(s: String) {
+//        println("hello $s")
+//    }
+//
+//}
+//
+//class TraceHelper(private val target: IClickListener  // 原始对象
+//) : InvocationHandler {
+//
+//    @Throws(Throwable::class)
+//    override fun invoke(proxy: Any, method: Method, args: Array<Any>): Any? {
+//        println("before print")
+//        method.invoke(target, args)  // 不加*, 就会有错: IllegalArgumentException: argument type mismatch
+//        println("after print")
+//        return null
+//    }
+//
+//}
+//
+//fun main(args: Array<String>) {
+//    val hello = enhanceHello(ButtonClickListener())
+//    hello.say("world")
+//}
+//
+//fun enhanceHello(target: IClickListener): IClickListener {
+//    return Proxy.newProxyInstance(
+//            IClickListener::class.java.classLoader,
+//            arrayOf<Class<*>>(IClickListener::class.java),
+//            TraceHelper(target)
+//    ) as IClickListener
+//}
 
 
 
--- KotlinPlayground/src/main/kotlin/ca/six/kex/function/vargs.kt
+++ KotlinPlayground/src/main/kotlin/ca/six/kex/function/vargs.kt
@@ -11,10 +11,16 @@ fun main(args: Array<String>) {
 */
 
 fun foo3(id: Int, vararg names: String) {
+    println("($id, $names, ${names.size}, ${names::class.java.simpleName})")
     names.forEach { println(it) }
 }
 
 fun main(args: Array<String>) {
     val ary = arrayOf("2", "a")
-    foo3(23, "1", *ary, "b", "c")
+    foo3(23, "1", *ary, "b", "c") //=> (23, [Ljava.lang.String;@4617c264, 5, String[])
+
+    println("=======")
+    foo3(21) //=> (21, [Ljava.lang.String;@36baf30c, 0, String[])
 }
+
+// 所以坑在这里: 虽然参数为空, 但这里的varargs仍不是空的. 这在反射时特别要注意, 这vararg得有一个空的array在才行
