888e1aab91c53e835bd2b06d246a515a4e8d84ba
songzhw <songzhw2012@gmail.com>
2018-06-09 00:33:51 -0400

    algorithm: calculate the point 24

--- Python101/src/algorithm/find24/Calculate24.py
+++ Python101/src/algorithm/find24/Calculate24.py
@@ -6,9 +6,56 @@ def calculate24(ary):
     pass
 
 
-if __name__ == '__main__':
-    calculate24([4, 10, 2, 5])
+"""
+:return Array<TreeNode>
+每个TreeNode都是一个树的根结点. 
+所以这其实是返回一系列树
+"""
 
+
+def findAllTrees(ary):
+    length = len(ary)
+
+    if length == 1:
+        node = TreeNode(ary[0])
+        return [node]  # 因为后面要遍历, 所以统一是返回一个list
+
+    trees = []
+    for i in range(1, length):
+        leftArray = ary[:i]
+        rightArray = ary[i:]
+        leftTrees = findAllTrees(leftArray)
+        rightTrees = findAllTrees(rightArray)
+        for left in leftTrees:
+            for right in rightTrees:
+                assembled = buildTrees(left, right)
+                trees.extend(assembled)
+
+    return trees
+
+
+def buildTrees(left, right):
+    trees = []
+    trees.append(TreeNode(operator="+", left=left, right=right))
+    trees.append(TreeNode(operator="-", left=left, right=right))
+    trees.append(TreeNode(operator="*", left=left, right=right))
+    trees.append(TreeNode(operator="/", left=left, right=right))  # TODO 分母不能为0
+    return trees
+
+
+if __name__ == '__main__':
+    ary = [4, 10, 2, 5]
+    calculate24(ary)
+
+    # ========= ========= ========= =========
+    trees = findAllTrees(ary)
+    for item in trees:
+        expression = "{} {} {}".format(item.left.num, item.operator, item.right.num)
+        result = eval(expression)
+        if result == 24 or result == 24.0: # math.isclose()也行
+            print(expression)
+
+    # ========= ========= ========= =========
     n1 = TreeNode(4)
     n2 = TreeNode(10)
     n3 = TreeNode(2)
@@ -27,4 +74,4 @@ if __name__ == '__main__':
     n23 = TreeNode(operator="/", left=n2, right=n3)
     n34 = TreeNode(operator="-", left=n1, right=n23)
     root = TreeNode(operator="*", left=n34, right=n4)
-    print("(2) : root = " + str(int(root.num)))
+    print("(2) : root = " + str(root.num))
--- Python101/src/algorithm/find24/Node.py
+++ Python101/src/algorithm/find24/Node.py
@@ -13,25 +13,3 @@ class TreeNode:
 
     def __repr__(self):
         return self.num if self.operator is None else self.operator
-
-
-class BinaryTree:
-    def __init__(self):
-        self.node = TreeNode()
-        self.left = None
-        self.right = None
-
-    # def __repr__(self):
-    #     if self.key in ['+', '-', '*', '/']:
-    #         return '<Node operator="{}">'.format(self.operator)
-    #     else:
-    #         return '<Node value="{}">'.format(self.result)
-
-    def setExpression(self, left, right, operator):
-        self.left = left
-        self.rigth = right
-        self.node.operator = operator
-
-        expression = "{} {} {}".format(left.node.num, operator, right.node.num)  # 2 * 3
-        self.node.num = eval(expression)  # 6
-        return self
