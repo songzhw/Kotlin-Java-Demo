1db3a1ecb30e5283c5260680fd30cbe8664d77f9
songzhw <songzhw2012@gmail.com>
2018-06-21 08:31:25 -0400

    java7 auto close

--- AdvancedJ/src/main/java/java7/autoclose/AutocloseDemo.java
+++ AdvancedJ/src/main/java/java7/autoclose/AutocloseDemo.java
@@ -1,8 +1,15 @@
 package java7.autoclose;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 public class AutocloseDemo {
 
@@ -15,9 +22,46 @@ public class AutocloseDemo {
         }
     }
 
-    String readFirstLine_java7(String path) throws IOException{
-        try(BufferedReader br = new BufferedReader(new FileReader(path))) {
+    String readFirstLine_java7(String path) throws IOException {
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             return br.readLine();
         }
     }
+
+    public void writeToFileZipFileContents(String src, String out) throws java.io.IOException {
+        Charset charset = java.nio.charset.StandardCharsets.US_ASCII;
+        Path outputFilePath = java.nio.file.Paths.get(out);
+
+        try (
+                ZipFile zf = new ZipFile(src);
+                BufferedWriter writer = Files.newBufferedWriter(outputFilePath, charset)
+        ) {
+            for (Enumeration entries = zf.entries(); entries.hasMoreElements(); ) {
+                String newLine = System.getProperty("line.separator");
+                String zipEntryName = ((ZipEntry) entries.nextElement()).getName() + newLine;
+                writer.write(zipEntryName, 0, zipEntryName.length());
+            }
+        }
+    }
+
+    public void custom() {
+//        try(String str = "abc") {}
+
+        try (MyResource res = new MyResource()) {
+            System.out.println(res);
+        }
+    }
+
+}
+
+class MyResource implements AutoCloseable {
+    String member = "xxx";
+
+    @Override
+    public void close() {
+        member = null;
+        System.out.println("MyResource close()");
+    }
+
+    // 另一可能 public void close() throws Exception {
 }
