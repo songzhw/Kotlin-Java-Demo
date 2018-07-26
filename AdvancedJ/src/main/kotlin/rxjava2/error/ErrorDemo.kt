99688277457f9a78b3d3ddce255174cb3caa784d
songzhw <songzhw2012@gmail.com>
2018-07-26 15:40:38 -0400

    rxjava2: error

--- AdvancedJ/src/main/kotlin/rxjava2/error/ErrorDemo.kt
+++ AdvancedJ/src/main/kotlin/rxjava2/error/ErrorDemo.kt
@@ -4,6 +4,6 @@ package rxjava2.error
 fun main(args: Array<String>) {
     var i = 1
     val http = HttpEngine()
-    http.getStoryList()
-            .subscribe{ value : String -> println("${i++}.$value")}
+    http.getStory_E()
+            .subscribe{ value -> println("${i++}.$value")}
 }
--- AdvancedJ/src/main/kotlin/rxjava2/error/HttpEngine.kt
+++ AdvancedJ/src/main/kotlin/rxjava2/error/HttpEngine.kt
@@ -3,8 +3,12 @@ package rxjava2.error
 import io.reactivex.Observable
 
 class HttpEngine {
-    fun getStoryList(): Observable<String> {
-        val list = arrayOf("One", "Two", "Three", "Four", "Five")
-        return Observable.fromArray(*list)
+    fun getStoryList(): Observable<List<String>> {
+        val list = arrayOf("One", "Two", "Three", "Four", "Five").asList()
+        return Observable.just(list)
+    }
+
+    fun getStory_E() : Observable<String>{
+        return Observable.error(Exception("test"))
     }
 }
