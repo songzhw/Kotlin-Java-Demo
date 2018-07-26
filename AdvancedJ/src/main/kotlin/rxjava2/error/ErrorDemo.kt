6faab7e19ac15f21e87f52126096dd6b35a6532a
songzhw <songzhw2012@gmail.com>
2018-07-26 16:08:01 -0400

    rxjava2: onErrorReturn

--- AdvancedJ/src/main/kotlin/rxjava2/error/ErrorDemo.kt
+++ AdvancedJ/src/main/kotlin/rxjava2/error/ErrorDemo.kt
@@ -1,9 +1,14 @@
 package rxjava2.error
 
+val DEFAULT_ERROR = 0
 
 fun main(args: Array<String>) {
     var i = 1
     val http = HttpEngine()
-    http.getStory_E()
-            .subscribe{ value -> println("${i++}.$value")}
+    http.getStoryList()
+            .map { 23/0 }
+            .onErrorReturn { error ->
+                println("szw error ${error}")
+                DEFAULT_ERROR }
+            .subscribe{ println("$it")}
 }
--- AdvancedJ/src/main/kotlin/rxjava2/error/HttpEngine.kt
+++ AdvancedJ/src/main/kotlin/rxjava2/error/HttpEngine.kt
@@ -3,12 +3,12 @@ package rxjava2.error
 import io.reactivex.Observable
 
 class HttpEngine {
+    fun getStory_E() : Observable<String>{
+        return Observable.error(Exception("test"))
+    }
+
     fun getStoryList(): Observable<List<String>> {
         val list = arrayOf("One", "Two", "Three", "Four", "Five").asList()
         return Observable.just(list)
-    }
-
-    fun getStory_E() : Observable<String>{
-        return Observable.error(Exception("test"))
     }
 }
