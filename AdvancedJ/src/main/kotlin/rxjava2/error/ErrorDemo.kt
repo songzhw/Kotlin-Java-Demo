b3fa5f47496b741fb08a3a33c224aea59b12ea07
songzhw <songzhw2012@gmail.com>
2018-07-28 22:08:15 -0400

    rxjava2: onErrorReturn, onErrorResumeNext

--- AdvancedJ/src/main/kotlin/rxjava2/error/ErrorDemo.kt
+++ AdvancedJ/src/main/kotlin/rxjava2/error/ErrorDemo.kt
@@ -1,19 +1,31 @@
 package rxjava2.error
 
 import io.reactivex.Observable
-import io.reactivex.ObservableSource
 import io.reactivex.Observer
-import io.reactivex.functions.Consumer
 import io.reactivex.functions.Function
 
 val DEFAULT_ERROR = 0
+val DEFAULT_ERROR_100 = 100
 
 fun main(args: Array<String>) {
     var i = 1
     val http = HttpEngine()
     http.getStoryList()
             .map { 23/0 }
-            .subscribe{ println("$it")}
+            .onErrorReturn { DEFAULT_ERROR }
+            .subscribe{ println("$it")}  //=> 0
+
+    http.getStoryList()
+            .map { 23/0 }
+            .onErrorResumeNext(Function {Observable.just(DEFAULT_ERROR_100)})
+            .subscribe{ println("$it")}  //=> 100
+
+
+}
+
+/*
+onErrorResumeNext()的两种形式:
+
 
     Observable.just(23)
             .onErrorResumeNext(object : Observable<Int>() {
@@ -24,6 +36,8 @@ fun main(args: Array<String>) {
             .subscribe { integer -> println("onNext() : " + integer!!) }
 
     Observable.just(23)
+            .map{ 24 / 0}
             .onErrorResumeNext(Function { Observable.just(300) })
             .subscribe { integer -> println("onNext() : " + integer!!) }
-}
+
+ */
