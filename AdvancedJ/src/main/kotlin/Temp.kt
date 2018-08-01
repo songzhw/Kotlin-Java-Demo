8e4517213b46fed85b28476a84c64e8fab671cf3
songzhw <songzhw2012@gmail.com>
2018-07-31 22:43:24 -0400

    kotlin high-order funciton

--- AdvancedJ/src/main/kotlin/Temp.kt
+++ AdvancedJ/src/main/kotlin/Temp.kt
@@ -4,16 +4,31 @@ class Bird {
         println("szw eat $food")
         return ""
     }
+
+    fun sleep(): String = "test"
+
+    fun zzz() = sleep()
+
+    fun zzzzz() = this::sleep
+
+    fun z(action: () -> String) {
+        action()
+    }
+
+
 }
 
 fun Bird.sing() = println("sing")
-fun Bird.sleep() : String = println("test")
 
-//fun Bird.lunch(food: String, action: (String) -> Unit): String = this.eat(food) {
-//    return@eat "done"
-//}
+fun Bird.lunch(food: String, action: (String) -> Unit): String = this.eat(food) {
+    action(food)
+}
 
 
 fun main(args: Array<String>) {
-    Bird().sing()
+    val bird = Bird()
+    bird.sing()
+    bird.zzz()
+    bird.zzzzz()
+    bird.z(bird::sleep)
 }
