e9fd5c03ddcec2a075a4d1cdcefe51531b453cb1
songzhw <songzhw2012@gmail.com>
2018-08-04 21:37:10 -0400

    find common ones

--- AdvancedJ/src/main/kotlin/algorithm/frequency/CommonInThreeAry.kt
+++ AdvancedJ/src/main/kotlin/algorithm/frequency/CommonInThreeAry.kt
@@ -10,19 +10,26 @@ fun main(args: Array<String>) {
     // Approach 1. for(for(for))自然是一个方法, 只是性能不好
 
     // Approach 2. 时间要从O(n3)要优化
-    var commons = HashSet<Char>()
+    val tmp1 = HashSet<Char>()
+    val tmp2 = HashSet<Char>()
+    val commons = HashSet<Char>()
 
     for(char in chars){
-        commons.add(char);
+        tmp1.add(char);
     }
 
     for(char in str.toCharArray()){
-        if(!commons.contains(char)){
-            println("remove $char")
-            commons.remove(char)
-            println("    set-> $commons")
+        if(tmp1.contains(char)){
+            tmp2.add(char)
         }
     }
+
+    for(char in list){
+        if(tmp2.contains(char)){
+            commons.add(char)
+        }
+    }
+
 
     println("set = $commons")
 
