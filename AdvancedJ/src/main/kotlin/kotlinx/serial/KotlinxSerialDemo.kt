d499c1b8909184b72f7d5bd7250b14a792a7e853
songzhw <songzhw2012@gmail.com>
2018-08-21 14:59:18 -0400

    customize serializer

--- AdvancedJ/src/main/kotlin/kotlinx/serial/KotlinxSerialDemo.kt
+++ AdvancedJ/src/main/kotlin/kotlinx/serial/KotlinxSerialDemo.kt
@@ -1,6 +1,7 @@
 package kotlinx.serial
 
 import kotlinx.serialization.json.JSON
+import java.util.*
 
 fun main(args: Array<String>) {
 //    val resp = """{"code":100,"msg":"success","payload":{"imgUrl":"http://192.168.2.26:8899/images/splash/splash3.jpg"}}"""
@@ -8,4 +9,16 @@ fun main(args: Array<String>) {
     val user1 = JSON.parse<User>(resp1)
     println("szw $user1")
 
+
+    var resp2 = JSON.stringify(User(122, "test", true))
+    println("szw $resp2")
+
+    var date = Calendar.getInstance().apply { set(2018, 8, 18, 14, 52, 10) }.time
+    println("szw $date")   //=> 即Date类型的toString()是: "Tue Sep 18 14:52:10 EDT 2018"的样式
+
+    /*
+    若没有自定义Date的解析方式, 那结果就是: {"code":200,"msg":"sueess","timestamp":"Tue Sep 18 14:52:10 EDT 2018"}
+    若定义了, 则结果是:
+     */
+    println(JSON.stringify(Response(200, "sueess", date)))
 }
---
+++ AdvancedJ/src/main/kotlin/kotlinx/serial/Response.kt
@@ -0,0 +1,10 @@
+package kotlinx.serial
+
+import kotlinx.serialization.Serializable
+import java.util.*
+
+// 不加@Serializable, 就会运行时报错: " Can't locate default serializer for class kotlinx.serial.Response (Kotlin reflection is not available)"
+@Serializable
+data class Response(val code : Int, val msg: String, val timestamp: Date){
+
+}
