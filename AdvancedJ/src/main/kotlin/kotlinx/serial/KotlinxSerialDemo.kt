a8123f3b402b6deaf2a0eabd5bd2083a8e267429
songzhw <songzhw2012@gmail.com>
2018-08-21 15:00:05 -0400

    customize serializer

--- AdvancedJ/src/main/kotlin/kotlinx/serial/KotlinxSerialDemo.kt
+++ AdvancedJ/src/main/kotlin/kotlinx/serial/KotlinxSerialDemo.kt
@@ -18,7 +18,7 @@ fun main(args: Array<String>) {
 
     /*
     若没有自定义Date的解析方式, 那结果就是: {"code":200,"msg":"sueess","timestamp":"Tue Sep 18 14:52:10 EDT 2018"}
-    若定义了, 则结果是:
+    若定义了, 则结果是:  {"code":200,"msg":"sueess","timestamp":"2018-09-18 14:52:10"}
      */
     println(JSON.stringify(Response(200, "sueess", date)))
 }
--- AdvancedJ/src/main/kotlin/kotlinx/serial/Response.kt
+++ AdvancedJ/src/main/kotlin/kotlinx/serial/Response.kt
@@ -5,6 +5,8 @@ import java.util.*
 
 // 不加@Serializable, 就会运行时报错: " Can't locate default serializer for class kotlinx.serial.Response (Kotlin reflection is not available)"
 @Serializable
-data class Response(val code : Int, val msg: String, val timestamp: Date){
+data class Response(val code : Int,
+                    val msg: String,
+                    @Serializable(with = DateSerializer::class) val timestamp: Date){
 
 }
