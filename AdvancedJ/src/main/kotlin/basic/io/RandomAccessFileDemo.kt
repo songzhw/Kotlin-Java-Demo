efa39c1502cfa1dbf3db35b3b84579cc914e6548
songzhw <songzhw2012@gmail.com>
2018-09-02 18:45:08 -0400

    random access file

--- AdvancedJ/src/main/kotlin/basic/io/RandomAccessFileDemo.kt
+++ AdvancedJ/src/main/kotlin/basic/io/RandomAccessFileDemo.kt
@@ -7,9 +7,14 @@ fun readFile() {
     val stream = RandomAccessFile("build.gradle", "rw")
     println("first position = ${stream.filePointer}")
 
-    stream.seek(6)
-    println("then position = ${stream.filePointer}")
+    //在最末追回一个注释
+    val comment = "\n// added by RandomAccessFile demo"
+    stream.seek(stream.length())
+    stream.write(comment.toByteArray())  //java中是用"write(comment.getBytes())"
 
+
+    // 要开始读取文件, 就得再移支指针到最开始来
+    stream.seek(0)
     val buff = ByteArray(1024)
     var readedCount = stream.read(buff)
 
@@ -19,6 +24,7 @@ fun readFile() {
     }
 
 }
+
 
 
 fun main(args: Array<String>) {
