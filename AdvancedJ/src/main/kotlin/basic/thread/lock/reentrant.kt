d5d5f3927be244b0fde39efa0de5bf8343ab7474
songzhw <songzhw2012@gmail.com>
2018-09-16 15:18:14 -0400

    reentrant lock

--- AdvancedJ/src/main/kotlin/basic/thread/lock/reentrant.kt
+++ AdvancedJ/src/main/kotlin/basic/thread/lock/reentrant.kt
@@ -8,20 +8,32 @@ class Target {
     @Synchronized fun inner() {}
 }
 
-class Lock {
-    private var isLocked = false
-    private val lock = java.lang.Object()
+class MyLock {
+    val lock = java.lang.Object();
+    var isLocked = false
+    var lockedBy: Thread? = null
+    var lockedCount = 0
 
-    @Synchronized fun lock(){
-        while(isLocked){
-            lock.wait()
+    @Synchronized fun lock() {
+        val current = Thread.currentThread()
+
+        while (isLocked && lockedBy !== current) {
+            lock.wait() //要写在synchornized里, 不然会crash
         }
         isLocked = true
+        lockedCount++
+        lockedBy = current
     }
 
-    @Synchronized fun unlock(){
-        isLocked = false
-        lock.notify()
+    @Synchronized fun unlock() {
+        if (Thread.currentThread() === this.lockedBy) {
+            lockedCount--
+
+            if (lockedCount == 0) {
+                isLocked = false //上面是while(A & B), 用的是&, 所以这里不让lockedBy = null也是行的
+                lock.notify() //要写在synchornized里, 不然会crash
+            }
+        }
+
     }
 }
-
