de2f8083842a60d2fa1c8eefaaf10b8b288a4a2f
songzhw <songzhw2012@gmail.com>
2018-09-16 15:11:25 -0400

    unreentrant lock

--- AdvancedJ/src/main/java/thread/lock/Target.java
+++ AdvancedJ/src/main/java/thread/lock/Target.java
@@ -8,3 +8,19 @@ public class Target {
     synchronized public void inner() {
     }
 }
+
+class Target2{
+    MyLock lock = new MyLock();
+
+    public void outer() throws InterruptedException {
+        lock.lock();
+        inner();
+        lock.unlock();
+    }
+
+    public void inner() throws InterruptedException {
+        lock.lock();
+        // do something
+        lock.unlock();
+    }
+}
