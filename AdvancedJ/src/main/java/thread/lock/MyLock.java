1a7c14126bf31c0117a5daac455d854a86524ea5
songzhw <songzhw2012@gmail.com>
2018-09-16 15:15:42 -0400

    reentrant lock

--- AdvancedJ/src/main/java/thread/lock/MyLock.java
+++ AdvancedJ/src/main/java/thread/lock/MyLock.java
@@ -1,18 +1,31 @@
 package thread.lock;
 
 class MyLock {
-    private boolean isLocked = false;
+    boolean isLocked = false;
+    Thread lockedBy = null;
+    int lockedCount = 0;
 
     synchronized public void lock() throws InterruptedException {
-        while (isLocked) {
+        Thread current = Thread.currentThread();
+
+        while (isLocked && lockedBy != current) {
             this.wait(); //要写在synchornized里, 不然会crash
         }
         isLocked = true;
+        lockedCount++;
+        lockedBy = current;
     }
 
     synchronized public void unlock() throws InterruptedException {
-        isLocked = false;
-        this.notify(); //要写在synchornized里, 不然会crash
+        if(Thread.currentThread() == this.lockedBy){
+            lockedCount--;
+
+            if(lockedCount == 0){
+                isLocked = false; //上面是while(A & B), 用的是&, 所以这里不让lockedBy = null也是行的
+                this.notify(); //要写在synchornized里, 不然会crash
+            }
+        }
+
     }
 }
 
