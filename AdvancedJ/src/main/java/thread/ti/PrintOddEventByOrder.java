61a2c2d84557639bcec61bc97d24bb78433447a1
songzhw <songzhw2012@gmail.com>
2018-09-18 09:39:52 -0400

    finally success

--- AdvancedJ/src/main/java/thread/ti/PrintOddEventByOrder.java
+++ AdvancedJ/src/main/java/thread/ti/PrintOddEventByOrder.java
@@ -1,31 +1,39 @@
 package thread.ti;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 // 两个线程交替打印 1 - 20
 public class PrintOddEventByOrder {
 
     public static void main(String[] args) {
         final Object lock1 = new Object();
         final Object lock2 = new Object();
+        AtomicBoolean isOneRun = new AtomicBoolean(false);
 
         Thread t1 = new Thread(() -> {
-            for (int i = 1; i < 20; i += 2) {
+            for (int i = 1; i < 10; i += 2) {
                 System.out.println("===> " + i);
+                isOneRun.set(true);
 
                 anotify(lock2);
-                if (i != 19) {
+                if (i != 9) {
                     await(lock1);
                 }
             }
         });
 
         Thread t2 = new Thread(() -> {
-            for (int i = 2; i <= 20; i += 2) {
-                if (i != 20) {
-                    await(lock2);
-                }
+            if(!isOneRun.get()){
+                await(lock2);
+            }
+
+            for (int i = 2; i <= 10; i += 2) {
                 System.out.println("*** " + i);
 
                 anotify(lock1);
+                if (i != 10) {
+                    await(lock2);
+                }
             }
         });
 
