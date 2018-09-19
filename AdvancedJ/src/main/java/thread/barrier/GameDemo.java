d3b39bcd18a7f2b20d612b22bba044da57d0163c
songzhw <songzhw2012@gmail.com>
2018-09-19 12:04:38 -0400

    CyclicBarrier

--- AdvancedJ/src/main/java/thread/barrier/GameDemo.java
+++ AdvancedJ/src/main/java/thread/barrier/GameDemo.java
@@ -4,11 +4,13 @@ import java.util.concurrent.CyclicBarrier;
 
 public class GameDemo {
     public static void main(String[] args) {
-        CyclicBarrier barrier = new CyclicBarrier(3);
+        CyclicBarrier barrier = new CyclicBarrier(3, () -> {
+            System.out.println("action in CyclicBarrier - "+Thread.currentThread().getName());
+        });
 
-        Thread p1 = new Thread(new Player("player01", barrier));
-        Thread p2 = new Thread(new Player("player02", barrier));
-        Thread p3 = new Thread(new Player("player03", barrier));
+        Thread p1 = new Thread(new Player("player01", barrier),"t1");
+        Thread p2 = new Thread(new Player("player02", barrier),"t2");
+        Thread p3 = new Thread(new Player("player03", barrier),"t3");
 
         p1.start();
         p2.start();
