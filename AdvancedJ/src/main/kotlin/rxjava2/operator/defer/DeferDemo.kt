2c7581094b98390c0d1980168491fc8021b6ee45
songzhw <songzhw2012@gmail.com>
2018-10-18 10:10:22 -0400

    Observable.create() and one real life sample

--- AdvancedJ/src/main/kotlin/rxjava2/operator/defer/DeferDemo.kt
+++ AdvancedJ/src/main/kotlin/rxjava2/operator/defer/DeferDemo.kt
@@ -19,8 +19,13 @@ fun justDemo() {
 
 }
 
-class Corgi(var name: String){
-    fun observe() : Observable<String> {
+// = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+
+/*
+defer(callable) : 在发生订阅关系时，callable.call();
+ */
+class Corgi(var name: String) {
+    fun observe(): Observable<String> {
         // defer参数是一个Callable, 其实是public interface Callable<V> {    V call() ;   }
         // defer的这个Callable限定死了得返回一个Observable<T>
         return Observable.defer {
@@ -29,16 +34,66 @@ class Corgi(var name: String){
     }
 }
 
-fun deferDemo(){
+fun deferDemo() {
     val dog = Corgi("one")
 
     val observable = dog.observe()
 
     dog.name = "two"
 
-    observable.subscribe { println("dog name = $it") } //=> two
+    observable.subscribe { println("Corgi name = $it") } //=> two
+
 }
 
+// = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+
+class ShibaInu(var name: String) {
+    fun observe(): Observable<String> {
+        return Observable.create { emitter ->
+            emitter.onNext(name)
+            emitter.onComplete()
+        }
+    }
+}
+
+fun createDemo() {
+    val dog = ShibaInu("one")
+
+    val observable = dog.observe()
+
+    dog.name = "two"
+
+    observable.subscribe { println("ShibaInu name = $it") } //=> two
+
+}
+
+// = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+// 一个更真实的例子: 只有真正订阅了时, 才去存数据
+object db {
+    fun <T> writeToDisk(t : T) {}
+    fun close(){}
+}
+
+fun saveData(valueToSave: String) : Observable<String>{
+    try {
+        db.writeToDisk(valueToSave)
+    } catch(e : Exception){
+        return Observable.error(e)
+    } finally {
+        db.close()
+    }
+
+    return Observable.just("success")
+}
+
+
+// = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 fun main(args: Array<String>) {
+    println(" - - - - - - - - - - - - ")
+    justDemo()
+    println(" - - - - - - - - - - - - ")
     deferDemo()
+    println(" - - - - - - - - - - - - ")
+    createDemo()
+    println(" - - - - - - - - - - - - ")
 }
