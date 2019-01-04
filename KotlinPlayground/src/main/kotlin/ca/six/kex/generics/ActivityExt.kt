9c11bda2f58fedf232def3c09bd9a0bb92903ae5
songzhw <songzhw2012@gmail.com>
2019-01-04 09:02:52 -0500

    kotlin reified -> java

--- KotlinPlayground/src/main/kotlin/ca/six/kex/generics/ActivityExt.kt
+++ KotlinPlayground/src/main/kotlin/ca/six/kex/generics/ActivityExt.kt
@@ -5,3 +5,13 @@ import ca.six.jold.generics.helper.Activity
 inline fun <reified T: Activity> Activity.jump(){
     startActivity(this, T::class.java)
 }
+
+/*
+编译后是:
+public final class ActivityExtKt {
+   private static final void jump(Activity $receiver) {
+      Intrinsics.reifiedOperationMarker(4, "T");
+      $receiver.startActivity($receiver, Activity.class);
+   }
+}
+ */
