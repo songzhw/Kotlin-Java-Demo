fad5250e5ae081c8fd3a92623c562fe8efdb05b5
songzhw <songzhw2012@gmail.com>
2019-02-20 17:40:02 -0500

    heater for more

--- algo/src/main/java/ca/six/algo/bisearch/HeaterRadius.java
+++ algo/src/main/java/ca/six/algo/bisearch/HeaterRadius.java
@@ -17,7 +17,7 @@ class HeaterRadius {
         HeaterRadius obj = new HeaterRadius();
 
         int[] houses = new int[]{1, 2, 3, 4, 5};
-        int[] heaters = new int[]{4};
+        int[] heaters = new int[]{2, 4};
         int radius = obj.getRadius(houses, heaters);
         System.out.println("szw radius = " + radius);
 
@@ -30,8 +30,26 @@ class HeaterRadius {
             int left = index;
             int right = houses.length - 1 - index;
             return Math.max(left, right);
+        } else {
+            int max = 0;
+            for (int i = 0; i < (heatersLength - 1); i++) {
+                int heater = heaters[i];
+                int index = getIndex(houses, heater);
+                int prevIndex, nextIndex;
+                if (i == 0) {
+                    prevIndex = 0;
+                    nextIndex = getIndex(houses, heaters[i+1]);
+                } else if (i == houses.length - 1) {
+                    prevIndex = getIndex(houses, heaters[i-1]);
+                    nextIndex = houses.length - 1;
+                } else {
+                    prevIndex = getIndex(houses, heaters[i-1]);
+                    nextIndex = getIndex(houses, heaters[i+1]);
+                }
+                max = Math.max(index - prevIndex, nextIndex - index);
+            }
+            return max;
         }
-        return 1;
     }
 
     int getIndex(int[] houses, int heater) {
