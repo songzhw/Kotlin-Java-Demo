963e4a8158f832cae2c23ee41d3148e9fc486839
songzhw <songzhw2012@gmail.com>
2019-03-15 17:42:11 -0400

    why AtomicInteger could make this problem disappear?

---
+++ AdvancedJ/src/main/java/thread/CAS/MyAtomicInt.java
@@ -0,0 +1,28 @@
+package thread.CAS;
+
+class MyAtomicInt {
+//    private volatile int value;
+//
+//    public MyAtomicInt(int initialValue) {
+//        value = initialValue;
+//    }
+//
+//    public int plusAndGet() {
+//        for (; ; ) {
+//            int current = get();
+//            int next = current + 1;
+//            if (compareAndSet(current, next)) {
+//                return next;
+//            }
+//        }
+//    }
+//
+//    public final int get() {
+//        return value;
+//    }
+//
+//    public native boolean compareAndSet(int expect, int updated) {
+//        // ...
+//    }
+
+}
