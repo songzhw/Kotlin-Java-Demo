f0b8c593f8d3ee5fda420be3d2fd08884ab89ac4
songzhw <songzhw2012@gmail.com>
2019-05-21 16:16:11 -0400

    add key2

--- KotlinPlayground/src/main/java/ca/six/jold/temp/Key1.java
+++ KotlinPlayground/src/main/java/ca/six/jold/temp/Key1.java
@@ -11,14 +11,24 @@ class Key1 {
     MessageDigest sha = MessageDigest.getInstance("SHA-256");
     sha.update(combined.getBytes(StandardCharsets.UTF_8));
     byte[] sha256 = sha.digest();
-    System.out.println(bytesToHex(sha256));
 
+
+    System.out.println(bytesToHex(sha256));
     StringBuilder sb = new StringBuilder();
     for (int i = 0; i < sha256.length; i++) {
       sb.append("_");
       sb.append(sha256[i]);
     }
     System.out.println(sb.toString());
+
+
+
+    byte[] resultBytes = new byte[16];
+    for (int x = sha256.length - 16; x < sha256.length; x++) {
+      resultBytes[x - (sha256.length - 16)] = sha256[x];
+    }
+    System.out.println(bytesToHex(resultBytes));
+
   }
 
   private static String bytesToHex(byte[] hash) {
@@ -28,8 +38,6 @@ class Key1 {
       if (hex.length() == 1) hexString.append('0');
       hexString.append(hex);
     }
-    System.out.println(hash.length);
-    System.out.println(hexString.toString().length());
     return hexString.toString();
   }
 }
---
+++ KotlinPlayground/src/main/java/ca/six/jold/temp/Key2.java
@@ -0,0 +1,7 @@
+package ca.six.jold.temp;
+
+class Key2 {
+  public static void main(String[] args) {
+
+  }
+}
