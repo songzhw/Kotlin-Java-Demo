c8ad741b59d7caa3caa3349308bd886cc2833576
songzhw <songzhw2012@gmail.com>
2019-05-21 16:28:47 -0400

    crash: javax.crypto.IllegalBlockSizeException: Input length not multiple of 16 bytes

--- KotlinPlayground/src/main/java/ca/six/jold/temp/Key2.java
+++ KotlinPlayground/src/main/java/ca/six/jold/temp/Key2.java
@@ -1,7 +1,23 @@
 package ca.six.jold.temp;
 
-class Key2 {
-  public static void main(String[] args) {
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
+
+class Key2 {
+  public static void main(String[] args) throws Exception {
+    String src = "easy plaintext";
+    String key = "0123456789abcdef";
+    byte[] raw = key.getBytes("utf-8");
+    SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
+
+    Cipher cipher = Cipher.getInstance("AES/ECB/NoPadding");
+    cipher.init(Cipher.ENCRYPT_MODE, skeySpec);
+    byte[] encrypted = cipher.doFinal(src.getBytes("utf-8"));
+    String result = Base64.getEncoder().encodeToString(encrypted);
+    System.out.println(result);
   }
 }
