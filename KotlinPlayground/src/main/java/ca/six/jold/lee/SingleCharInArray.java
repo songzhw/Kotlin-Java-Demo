57846fb0a63c347113498bdcf9d175e4196d2388
songzhw <songzhw2012@gmail.com>
2019-05-27 11:02:35 -0400

    xor

---
+++ KotlinPlayground/src/main/java/ca/six/jold/lee/SingleCharInArray.java
@@ -0,0 +1,31 @@
+package ca.six.jold.lee;
+
+// 从数组中找出唯一一个不重复的数字来
+class SingleCharInArray {
+  public static void main(String[] args) {
+    duplicateEven();
+    duplicateOdd();
+  }
+
+  // 好处是: 时间复杂度O(n), 空间复杂度O(1)
+  // 坏处是: 只能处理重复次数为偶数的情形
+  public static void duplicateEven(){
+    int[] ary = new int[]{2, 3, 2, 3, 4};
+    int ret = ary[0]; //从0号开始, 所以下面for循环就是从1开始. 总之就是所有数字全异或一下
+    for (int i = 1; i < ary.length; i++) {
+      ret ^= ary[i];  // ^是异或. 即"相同为0, 不同为1"
+    }
+    System.out.println("the result = " + ret); //=> 4
+
+    /*
+    解析: 因为相同为0, 所以那些重复的(偶数次重复)异或之后, 肯定全是为0
+    这样就成了0与那个单独的数再异或了, 如0与4(100), 就成了100; 0与3(11), 就成了11
+    这样就成了: 全部异或一次后, 结果就是那个单独出来不重复的数字
+     */
+  }
+
+  public static void duplicateOdd(){
+    // 若仍用duplicateEven的方法, 那结果就是6, 这明显是不对的.
+    int[] ary = new int[]{2, 3, 2, 3, 4};
+  }
+}
