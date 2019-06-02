6db64ef423c3ecc523e7f1bd649aa50a691fb596
songzhw <songzhw2012@gmail.com>
2019-06-01 20:12:44 -0400

    download - java7 NIO2

--- AdvancedJ/src/main/kotlin/basic/io/DownloadDemo.kt
+++ AdvancedJ/src/main/kotlin/basic/io/DownloadDemo.kt
@@ -8,6 +8,11 @@ import java.net.URL
 import java.nio.channels.Channels
 import java.nio.channels.FileChannel
 import java.nio.channels.ReadableByteChannel
+import java.io.IOException
+import java.nio.file.StandardCopyOption
+import java.nio.file.Files
+import com.sun.org.apache.xerces.internal.util.DOMUtil.getParent
+import java.nio.file.Paths
 
 
 fun download1_stream(url: String, saveDir: String, fileName: String) {
@@ -49,7 +54,18 @@ fun download2_NIO(url: String, saveDir: String, fileName: String) {
     }
 }
 
+fun download_NIO2(url: String, saveDir: String, fileName: String) {
+    // Kotlin的use:  会自动关闭调用者（无论中间是否出现异常）
+    URL(url).openStream().use { ins ->
+        val target = Paths.get(saveDir, fileName)
+        Files.createDirectories(target.parent)
+        Files.copy(ins, target, StandardCopyOption.REPLACE_EXISTING)
+
+    }
+
+}
+
 fun main() {
     val url = "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png";
-    download2_NIO(url, ".", "g2.png")
+    download_NIO2(url, ".", "g3.png")
 }
