b75e0c4139254935352b49662ff1eb29ce8e6558
songzhw <songzhw2012@gmail.com>
2019-06-19 09:42:28 -0400

    java float

--- KotlinPlayground/src/main/java/ca/six/jold/Temp.java
+++ KotlinPlayground/src/main/java/ca/six/jold/Temp.java
@@ -5,10 +5,18 @@ import java.util.Locale;
 import java.util.TimeZone;
 
 class Temp {
-    public static void main(String[] args) {
-      String ab = "ab";
+  public static void main(String[] args) {
+    System.out.println(8 | 1);
+    System.out.println(Integer.valueOf(Integer.toBinaryString(8)));
+//    System.out.println(Integer.valueOf(Double.toBinaryString(8.1)));
 
-    }
+    System.out.println(2 + 1);    //=> 3
+    System.out.println(2 + 0.1);  //=> 2.1
+    System.out.println(0.2 + 0.1);//=> 0.30000000000000004
+
+    System.out.println((0.1 + 0.2) + 0.3); //=> 0.6000000000000001
+    System.out.println(0.1 + (0.2 + 0.3)); //=> 0.6
+  }
 }
 
 /*
