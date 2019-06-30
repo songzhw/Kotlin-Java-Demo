16239aabdb78f49e6168388cfc320a2143d5df48
songzhw <songzhw2012@gmail.com>
2019-06-30 19:22:01 -0400

    first non-repeat;  failed, because the first count = 1 does not mean it has no duplicated item later

---
+++ AdvancedJ/src/main/kotlin/algorithm/array/FirstNonrepeat.kt
@@ -0,0 +1,12 @@
+package algorithm.array
+
+fun findFirstNonRepeated(ary: IntArray): Int {
+    val map = hashMapOf<Int, Int>()
+
+    return 1
+}
+
+fun main() {
+    val num = findFirstNonRepeated(intArrayOf(1, 1 , 3, 4, 5, 7, 4, 7, 5, 0, 5))
+    println(num)
+}
