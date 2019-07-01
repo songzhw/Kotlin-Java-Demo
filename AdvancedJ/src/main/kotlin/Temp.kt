9d8b7c76a0530b3e79140e4f7ab64a29096963c7
songzhw <songzhw2012@gmail.com>
2019-07-01 11:03:24 -0400

    竟然hashmap全是 [k-null]

--- AdvancedJ/src/main/kotlin/Temp.kt
+++ AdvancedJ/src/main/kotlin/Temp.kt
@@ -4,4 +4,30 @@ fun main(args: Array<String>) {
         println(c)
     } //=> ? ? c l e f
     println(str.length) //=>6
+    println("- - - - - - - - - ")
+
+    val charId = str.codePointAt(0)
+    val achar = Character.toChars(charId)
+    println(achar) //=> 𝄞
+    println("- - - - - - - - - ")
+
+    println(str.codePointCount(0, str.length)) //=> 5.  [begin, end)的范围
+    for (i in 0 until str.length) {
+        val codePoint = str.codePointAt(i)
+        val theChar = Character.toChars(codePoint)
+        println(theChar)
+    } //=>  𝄞 ? c l e f
+    println("- - - - - - - - - ")
+
+
+    val len = str.length
+    var i = 0
+    while(i < len){
+        val codePoint = str.codePointAt(i)
+        val charLength = Character.charCount(codePoint)
+        i += charLength
+        val theChar = Character.toChars(codePoint)
+        println(theChar)
+    }//=>  𝄞 c l e f
+
 }
--- AdvancedJ/src/main/kotlin/algorithm/strings/FirstNonrepeatChar.kt
+++ AdvancedJ/src/main/kotlin/algorithm/strings/FirstNonrepeatChar.kt
@@ -1,13 +1,36 @@
 package algorithm.strings
 
-fun findFirstNonRepeatedChar(str: String): String {
-    val map = hashMapOf<Int, Int>()
+fun findFirstNonRepeatedChar(str: String): CharArray {
+    val map = hashMapOf<CharArray, Int>()
     // for (c in str) {  // c只是一个字符, 但有时需要2个char来表示一个Utf-16字符(code point). 所以这种for-each不能用了
     val len = str.codePointCount(0, str.length)
-    for(i in 0..len){
+    var i = 0
+    while (i < len) {
+        val codePoint = str.codePointAt(i)
+        val charLength = Character.charCount(codePoint)
+        i += charLength
+        val theChar = Character.toChars(codePoint)
 
+        val count = if (map.get(theChar) != null) map.get(theChar) else 0;
+        val newCount = count!! + 1
+        map.set(theChar, newCount)
     }
-    return ""
+
+    i = 0
+    while (i < len) {
+        val codePoint = str.codePointAt(i)
+        val charLength = Character.charCount(codePoint)
+        i += charLength
+        val theChar = Character.toChars(codePoint)
+
+        val count = map.get(theChar)
+        println("${theChar} : $count")
+        if (count == 1) {
+            return theChar
+        }
+    }
+
+    return "".toCharArray()
 }
 
 fun main() {
