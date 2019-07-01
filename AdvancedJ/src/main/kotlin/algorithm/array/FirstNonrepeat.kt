f11712d15b61941b5e2fc81d2c7da7d0d2184bd0
songzhw <songzhw2012@gmail.com>
2019-07-01 10:15:38 -0400

    first non-repeat

--- AdvancedJ/src/main/kotlin/algorithm/array/FirstNonrepeat.kt
+++ AdvancedJ/src/main/kotlin/algorithm/array/FirstNonrepeat.kt
@@ -2,11 +2,22 @@ package algorithm.array
 
 fun findFirstNonRepeated(ary: IntArray): Int {
     val map = hashMapOf<Int, Int>()
+    for (num in ary) {
+        val count = if (map.get(num) != null) map.get(num) else 0;
+        val newCount = count!! + 1
+        map.set(num, newCount)
+    }
 
-    return 1
+    for (num in ary) {
+        val count = map.get(num)
+        if (count == 1) {
+            return num;
+        }
+    }
+    return -1
 }
 
 fun main() {
-    val num = findFirstNonRepeated(intArrayOf(1, 1 , 3, 4, 5, 7, 4, 7, 5, 0, 5))
-    println(num)
+    val num = findFirstNonRepeated(intArrayOf(1, 1, 3, 4, 5, 7, 4, 7, 5, 0, 5, 3))
+    println(num) //=> 0
 }
