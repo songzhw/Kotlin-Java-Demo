b2024f88b0d3681e0ac8166b964ac7e61b3ddc6b
songzhw <songzhw2012@gmail.com>
2019-07-05 21:17:22 -0400

    utf-16

--- AdvancedJ/src/main/kotlin/Temp.kt
+++ AdvancedJ/src/main/kotlin/Temp.kt
@@ -1,6 +1,9 @@
 fun main() {
-    val str = "𝄞clef"
+    val str :String= "𝄞clef"
     for(c in str){
         println(c)
     } //=> ? ? c l e f
+
+    val ary = charArrayOf()
+    val s = String(ary)
 }
---
+++ AdvancedJ/src/main/kotlin/algorithm/strings/RemoveChars.kt
@@ -0,0 +1,16 @@
+package algorithm.strings
+
+import java.util.*
+
+fun remove(src: String, delete: String): String {
+    val bits = BitSet()
+    for (c in delete.toCharArray()) {
+
+    }
+    return ""
+}
+
+fun main() {
+    val c = 'a'
+    println(c.toInt())
+}
