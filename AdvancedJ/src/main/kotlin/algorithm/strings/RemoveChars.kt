3a9b46dbe0de79e3f4a3643cb49f9987711f8d4a
songzhw <songzhw2012@gmail.com>
2019-07-05 21:24:12 -0400

    error : java.lang.ArrayIndexOutOfBoundsException: 0

--- AdvancedJ/src/main/kotlin/algorithm/strings/RemoveChars.kt
+++ AdvancedJ/src/main/kotlin/algorithm/strings/RemoveChars.kt
@@ -3,14 +3,31 @@ package algorithm.strings
 import java.util.*
 
 fun remove(src: String, delete: String): String {
-    val bits = BitSet()
+    val bits = BitSet(128)
     for (c in delete.toCharArray()) {
-
+        val index = c.toInt()
+        bits.set(index)
     }
-    return ""
+
+    val chars = charArrayOf()
+    var i = 0;
+    for (c in src) {
+        val index = c.toInt()
+        if (!bits.get(index)) { //if is not set
+            chars[i] = c
+            i++
+        }
+    }
+    return String(chars)
 }
 
 fun main() {
-    val c = 'a'
-    println(c.toInt())
+//    println(remove("java coffee", "aof"))
+    val chars = charArrayOf()
+    chars[0] = 'a'
 }
+
+/*
+    val c = 'a'
+    println(c.toInt()) //=>97
+ */
