196c3bb3f972bfc17aed159056410a7212388452
songzhw <songzhw2012@gmail.com>
2019-07-07 15:33:57 -0400

    kotlin and

--- AdvancedJ/src/main/kotlin/Temp.kt
+++ AdvancedJ/src/main/kotlin/Temp.kt
@@ -1,3 +1,9 @@
+fun is2Power(n: Int) {
+    if (n and n - 1 == 0) {
+        println("if")
+    }
+}
+
 fun main() {
     println(32 shl 2) //=> 128
 
@@ -7,4 +13,5 @@ fun main() {
     // >>> 无符号右移
     println(-32 shr 2) //=> -8
     println((-32).ushr(2))//=> 1073741816
+
 }
