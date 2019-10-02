fe3178b0d281621e44d826e6cce2bd9e6c91c1f0
songzhw <songzhw2012@gmail.com>
2019-10-02 09:25:27 -0400

    List<? extends *>

---
+++ Advanced_kb/src/generics/variant/ListVariant.java
@@ -0,0 +1,29 @@
+package generics.variant;
+
+import generics.data.Animal;
+import generics.data.Cat;
+import generics.data.Dog;
+import generics.data.Vegatable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class ListVariant {
+    public static void main(String[] args) {
+        Vegatable cabbige = new Vegatable();
+        Animal cow = new Animal();
+        Cat cat = new Cat();
+        Dog dog = new Dog();
+
+        List<? extends Animal> list = new ArrayList<>();
+        list.add(cabbige);
+        list.add(cow);
+        list.add(cat);
+        list.add(dog);
+        Animal anim = list.get(0);
+        Cat cat2 = list.get(1);
+
+        list = new ArrayList<Cat>();
+
+    }
+}
