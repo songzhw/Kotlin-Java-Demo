b2ffad5bc71d48017ad59323bc86bd6554062cdc
songzhw <songzhw2012@gmail.com>
2019-10-03 12:52:27 -0400

    solved

--- Advanced_kb/src/threads/PrintFooBarAlternatively.java
+++ Advanced_kb/src/threads/PrintFooBarAlternatively.java
@@ -1,28 +1,42 @@
 package threads;
 
+import java.util.concurrent.Semaphore;
+
 class FooBar {
     private int n;
+    private Semaphore seFoo = new Semaphore(1);
+    private Semaphore seBar = new Semaphore(0);
 
     public FooBar(int n) {
         this.n = n;
     }
 
     public void foo() {
-        for (int i = 0; i < n; i++) {
-            System.out.println("foo");
+        try {
+            for (int i = 0; i < n; i++) {
+                seFoo.acquire();
+                System.out.print("foo");
+                seBar.release();
+            }
+        } catch (Exception e) {
         }
     }
 
     public void bar() {
-        for (int i = 0; i < n; i++) {
-            System.out.println("bar");
+        try {
+            for (int i = 0; i < n; i++) {
+                seBar.acquire();
+                System.out.println("bar");
+                seFoo.release();
+            }
+        } catch (Exception e) {
         }
     }
 }
 
 public class PrintFooBarAlternatively {
     public static void main(String[] args) {
-        int count = 3;
+        int count = 6;
         FooBar target = new FooBar(count);
 
         Thread a = new Thread(() -> {
@@ -31,7 +45,6 @@ public class PrintFooBarAlternatively {
 
         Thread b = new Thread(() -> {
             target.bar();
-
         });
 
         b.start();
