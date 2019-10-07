bcc7700308294a709039cded4bad21ffbc1a86a6
songzhw <songzhw2012@gmail.com>
2019-10-06 23:33:44 -0400

    generics issue

--- AdvancedJ/src/main/java/Temp.java
+++ AdvancedJ/src/main/java/Temp.java
@@ -1,66 +1,25 @@
-import java.io.RandomAccessFile;
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
-import java.util.concurrent.CompletableFuture;
-import java.util.function.Consumer;
-import java.util.function.Function;
+import java.util.ArrayList;
+import java.util.List;
 
-import io.reactivex.Observable;
-import io.reactivex.ObservableEmitter;
-import io.reactivex.ObservableOnSubscribe;
-import io.reactivex.ObservableSource;
-import io.reactivex.Observer;
+class Animal{}
+class Dog extends Animal{}
+class Cat extends Animal{}
 
-class A {
-    public String name = "Apple";
-    public Consumer<Integer> hello;
-
-    public A() {
-        hello = (Integer it) -> System.out.println(it + ": Hello " + this.name);
+class Adapter {
+    public void setData(List<Animal> animals){
     }
 }
 
-class B {
-    // 无法使用 private Consumer hello = A.hello ;  因为A中没有一个static方法叫Hello
-    public String name = "Banana";
-    public Consumer<Integer> hello;
-}
-
-
-public class Temp {
-    public static void unlikeJavascript() {
-        A obj1 = new A();
-        B obj2 = new B();
-        obj2.hello = obj1.hello;
-        obj2.hello.accept(20); //=> 20: Hello Apple
-    }
-
-    public void is2Power(int n){
-        if ((n & (n - 1)) == 0) {
-            System.out.println("if");
-        }
-    }
-
-
+public class Temp{
     public static void main(String[] args) {
-        System.out.println(32 << 2); //=> 128
 
-        System.out.println(32 >> 2); //=> 8
-        System.out.println(32 >>> 2);//=> 8
+        List<Cat> cats = new ArrayList<>();
+        cats.add(new Cat());
+        cats.add(new Cat());
 
-        // >>> 无符号右移
-        System.out.println(-32 >> 2); //=> -8
-        System.out.println(-32 >>> 2);//=> 1073741816
+        Adapter adapter = new Adapter();
+        adapter.setData(cats);
 
 
     }
 }
-
-
-
-
-
-
-
-
