b9bef4eb3dfd2340141cdddad2156193b999142f
songzhw <songzhw2012@gmail.com>
2019-10-21 15:39:52 -0400

    kotlin lambda

--- Advanced_kb/src/function/closure/CallbackOnJava.java
+++ Advanced_kb/src/function/closure/CallbackOnJava.java
@@ -4,11 +4,24 @@ interface ICallbackOnJava {
     public int callJava(String id);
 }
 
+interface ICallbackOnJava2 {
+    public int callJava2A(String id);
+
+    public int callJava2B(String id);
+}
+
 
 class CallbackOnJava {
     public void call(String arg, ICallbackOnJava callback) {
         System.out.println("szw java call(callbackJava) : " + callback.callJava(arg));
     }
 
+    public void call2A(String arg, ICallbackOnJava2 callback) {
+        System.out.println("szw java call(callbackJava) : " + callback.callJava2A(arg));
+    }
+
+    public void call2B(String arg, ICallbackOnJava2 callback) {
+        System.out.println("szw java call(callbackJava) : " + callback.callJava2B(arg));
+    }
 }
 
--- Advanced_kb/src/function/closure/CallbackOnKolint.kt
+++ Advanced_kb/src/function/closure/CallbackOnKolint.kt
@@ -1,8 +1,29 @@
 package function.closure
 
+
+
 fun main() {
     val javaObj = CallbackOnJava()
     javaObj.call("hello") { name ->
-        20
+        20  // 和下面的object: ICallbackOnJava2对比下, 它是不能有return的, 不然会报错.
     }
+
+    // = = = = = = = = = = = = = = = = = = = =
+
+    // ERROR: because the ICallbackOnJava2 interface has two functions (not one!!!)
+//    javaObj.call2A("2A") { name ->
+//        30
+//    }
+
+    javaObj.call2A("2A", object : ICallbackOnJava2 {
+        override fun callJava2A(id: String?): Int {
+            return 30
+        }
+
+        override fun callJava2B(id: String?): Int {
+            return 10
+        }
+
+    })
+
 }
