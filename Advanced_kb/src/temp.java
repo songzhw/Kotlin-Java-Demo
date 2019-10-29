6ab5a57507fc2430b825a8cf89dd59aa13eb7730
songzhw <songzhw2012@gmail.com>
2019-10-29 16:01:03 -0400

    finally succeed

--- Advanced_kb/src/temp.java
+++ Advanced_kb/src/temp.java
@@ -1,5 +1,13 @@
+import org.zeroturnaround.zip.ZipUtil;
+
 import java.io.*;
 import java.util.zip.*;
 
 class temp {
+    public static void main(String[] args) {
+        String fromFolder = "/Users/zsong/temp/epub_reader/html_unzipped";
+        String toFile = "/Users/zsong/temp/epub_reader/t11.zip";
+
+        ZipUtil.pack(new File(fromFolder), new File(toFile));
+    }
 }
