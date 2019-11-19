63afb6c17acb8a0da15d0544dad985303e374435
songzhw <songzhw2012@gmail.com>
2019-11-19 14:58:01 -0500

    log utils

--- Advanced_kb/src/Temp.kt
+++ Advanced_kb/src/Temp.kt
@@ -1,12 +1,16 @@
+import java.lang.StringBuilder
+
+fun log(prefix: String, vararg args: Any) {
+    val sb = StringBuilder(prefix)
+    sb.append(": ")
+    for (param in args) {
+        sb.append(param)
+        sb.append(", ")
+    }
+    sb.removeSuffix(",")
+    println(sb.toString())
+}
+
 fun main() {
-    val root = "/storage/emulated/0/android/data/com.some.android/files/8316fc740455e9eeed3dc644cfead4ab/epubs/"
-    val path =
-        "/storage/emulated/0/android/data/com.some.android/files/8316fc740455e9eeed3dc644cfead4ab/epubs/15989428-0244-4808-bc54-22f51961efbb-3/oebps/brow_9780385537865_epub_c81_r1.htm"
-    var result = path.removePrefix(root)
-    println(result)
-    val start = result.indexOf("/")
-    result = result.substring(start + 1)
-    println(result) // note that it has to be equal exactly, "A" and "a" are not equal here
-
-
+    log("szw", "one", "two")
 }
