6a2fda0e96aa901c81a328025485ede0df36c0f7
songzhw <songzhw2012@gmail.com>
2019-11-19 15:39:08 -0500

    method name is a pain in the A~

--- Advanced_kb/src/Temp.java
+++ Advanced_kb/src/Temp.java
@@ -4,8 +4,12 @@ import org.zeroturnaround.zip.ZipUtil;
 
 import javax.swing.*;
 import java.io.*;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ForkJoinPool;
 import java.util.zip.*;
 
@@ -14,5 +18,20 @@ class Temp {
 
     }
 
+    public List<String> getParameterNameJava8(Class clazz, String methodName) {
+        List<String> paramterList = new ArrayList<>();
+        Method[] methods = clazz.getDeclaredMethods();
+        for (Method method : methods) {
+            if (methodName.equals(method.getName())) {
+                Parameter[] params = method.getParameters();
+                for (Parameter parameter : params) {
+                    paramterList.add(parameter.getName());
+                }
+
+            }
+        }
+
+        return paramterList;
+    }
 }
 
--- Advanced_kb/src/Temp.kt
+++ Advanced_kb/src/Temp.kt
@@ -1,4 +1,4 @@
-import java.lang.StringBuilder
+import java.util.*
 
 fun log(prefix: String, vararg args: Any) {
     val sb = StringBuilder(prefix)
@@ -9,8 +9,32 @@ fun log(prefix: String, vararg args: Any) {
     }
     sb.removeSuffix(",")
     println(sb.toString())
+
+}
+
+fun getParameterNameJava8(clazz: Class<*>, methodName: String): List<String> {
+    val paramterList: MutableList<String> = ArrayList()
+    val methods = clazz.declaredMethods
+    for (method in methods) {
+        if (methodName == method.name) {
+            val params = method.parameters
+            for (parameter in params) {
+                paramterList.add(parameter.name)
+            }
+        }
+    }
+    return paramterList
+}
+
+class Test {
+    fun test(id: Int, name: String) {}
 }
 
 fun main() {
     log("szw", "one", "two")
+
+    val args = getParameterNameJava8(Test::class.java, "test")
+    for (arg in args) {
+        println(arg)
+    } //=> arg0, arg1
 }
