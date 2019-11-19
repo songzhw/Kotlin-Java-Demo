a9de96442d74d8783be1e2334edaa31984d301fb
songzhw <songzhw2012@gmail.com>
2019-11-19 16:45:06 -0500

    add test cases

--- Advanced_kb/src/threads/MyBlockingQueue.kt
+++ Advanced_kb/src/threads/MyBlockingQueue.kt
@@ -59,3 +59,25 @@ class MyBlockingQueue(val capacity: Int) {
 
 }
 
+fun main() {
+    val queue = MyBlockingQueue(3)
+    for (i in 0..5) {
+        thread {
+            println("szw before = ${Thread.currentThread().name}")
+            queue.enqueue(i)
+            println("szw after  = ${Thread.currentThread().name}")
+
+            Thread.sleep(i * 1000 + 1000L)
+            queue.dequeue()
+            println("szw release(${Thread.currentThread().name})")
+        }
+    }
+}
+/*
+结果: 一开始就执行了5个线程的before, 顺序自然是不定的. 我们这次是0, 2, 1, 4, 5, 3
+然后入了队列的(也就是有after的线程的), 是2, 0, 1
+最先就释放了0, 然后进来了4 (队列成了2, 4, 1)
+然后释放了1, 然后进来了5 (队列成了2, 4, 5)
+然后释放了2, 进入了3
+最终一定时间后, 4, 3, 5都被释放了
+ */
