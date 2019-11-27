5bd9c70b138a4e4f1612b47ba6685d62cfa99299
songzhw <songzhw2012@gmail.com>
2019-11-27 16:14:17 -0500

    hungrey singleton

---
+++ Advanced_kb/src/threads/singleton/HungrySingleton.java
@@ -0,0 +1,9 @@
+package threads.singleton;
+
+public class HungrySingleton {
+    private static HungrySingleton instance = new HungrySingleton();
+    private HungrySingleton(){}
+    public static HungrySingleton getInstance() {
+        return instance;
+    }
+}
