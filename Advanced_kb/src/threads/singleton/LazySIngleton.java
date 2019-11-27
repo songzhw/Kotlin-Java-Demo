ba2d7a52782ef283fea2f0a826d43fb9d81d99f2
songzhw <songzhw2012@gmail.com>
2019-11-27 16:14:55 -0500

    lazy singleton

--- Advanced_kb/src/threads/singleton/LazySIngleton.java
+++ Advanced_kb/src/threads/singleton/LazySIngleton.java
@@ -1,2 +1,17 @@
-package threads.singleton
+package threads.singleton;
+
+public class LazySIngleton {
+    private volatile static LazySIngleton singleton;
+    private LazySIngleton(){}
+    public static LazySIngleton getSingleton() {
+        if (singleton == null) {
+            synchronized (LazySIngleton.class) {
+                if (singleton == null) {
+                    singleton = new LazySIngleton();
+                }
+            }
+        }
+        return singleton;
+    }
+}
 
