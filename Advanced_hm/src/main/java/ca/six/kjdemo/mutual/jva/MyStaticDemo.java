d996b4e3f117901600929f12bfdf2570425147b0
songzhw <songzhw2012@gmail.com>
2019-12-21 11:40:01 -0500

    kotlin static

---
+++ Advanced_hm/src/main/java/ca/six/kjdemo/mutual/jva/MyStaticDemo.java
@@ -0,0 +1,10 @@
+package ca.six.kjdemo.mutual.jva;
+
+import ca.six.kjdemo.mutual.kt.MyStatic;
+
+class MyStaticDemo {
+    public static void main(String[] args) {
+        MyStatic.Companion.foo();
+        MyStatic.Companion.getId();
+    }
+}
---
+++ Advanced_hm/src/main/kotlin/ca/six/kjdemo/mutual/kt/MyStatic.kt
@@ -0,0 +1,10 @@
+package ca.six.kjdemo.mutual.kt
+
+class MyStatic {
+    companion object {
+        val id = 23;
+        fun foo() {
+            println("foo")
+        }
+    }
+}
