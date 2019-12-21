31becc053da3a66048e2436deae462b42e0fceaf
songzhw <songzhw2012@gmail.com>
2019-12-21 08:47:45 -0500

    @file:JvmMultifileClass

--- Advanced_hm/src/main/kotlin/ca/six/kjdemo/mutual/kt/ManyUtils.kt
+++ Advanced_hm/src/main/kotlin/ca/six/kjdemo/mutual/kt/ManyUtils.kt
@@ -1,15 +1,12 @@
+@file:JvmMultifileClass
+@file:JvmName("ManyUtils")
 package ca.six.kjdemo.mutual.kt
 
 fun String.firstOne(): String {
     return this.substring(0, 1)
 }
 
-fun Int.dollar(): String {
-    return "\$$this"
+fun Int.plus10(): Int {
+    return this + 10
 }
 
-fun main() {
-    val num = 10
-    println("szw $$num")  //=> szw $10
-    println("szw \$$num") //=> szw $10
-}
