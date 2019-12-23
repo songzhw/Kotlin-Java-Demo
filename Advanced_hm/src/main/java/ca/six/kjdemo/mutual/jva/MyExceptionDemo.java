1901951c841fa7528d19e5a81b8a79e9dd58c3fc
songzhw <songzhw2012@gmail.com>
2019-12-23 16:57:27 -0500

    throw exception

---
+++ Advanced_hm/src/main/java/ca/six/kjdemo/mutual/jva/MyExceptionDemo.java
@@ -0,0 +1,9 @@
+package ca.six.kjdemo.mutual.jva;
+
+import ca.six.kjdemo.mutual.kt.MyExceptionKt;
+
+class MyExceptionDemo {
+    public static void main(String[] args) {
+        MyExceptionKt.iThrow();
+    }
+}
---
+++ Advanced_hm/src/main/kotlin/ca/six/kjdemo/mutual/kt/MyException.kt
@@ -0,0 +1,9 @@
+package ca.six.kjdemo.mutual.kt
+
+import java.lang.Exception
+
+
+fun iThrow() {
+    println("szw iThrow()")
+    throw Exception("this is wrong")
+}
