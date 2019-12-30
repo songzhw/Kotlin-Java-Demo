0135e01a9ec6e5e9f9460d82d2cdb5d5550f22f1
songzhw <songzhw2012@gmail.com>
2019-12-30 15:30:02 -0500

    still no go

---
+++ Advanced_kb/src/rxjava2/ComplexScanWith.java
@@ -0,0 +1,40 @@
+package rxjava2;
+
+import io.reactivex.Observable;
+
+class Changex {
+    private final int id;
+
+    public Changex(int id) {
+        this.id = id;
+    }
+}
+
+class Eventx {
+    private final Changex changex;
+
+    public Eventx(Changex c) {
+        this.changex = c;
+    }
+}
+
+class ComplexScanWith {
+    public static void main(String[] args) {
+
+    }
+
+    public void work() {
+        Observable.just(new Changex(10))
+                .scanWith(this::one, this::two)
+                .subscribe();
+    }
+
+    public Eventx one() {
+        Changex c = new Changex(21);
+        return new Eventx(c);
+    }
+
+    public Eventx two(Changex c) {
+        return new Eventx(c);
+    }
+}
