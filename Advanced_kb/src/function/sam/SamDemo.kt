04c8b30a4cf65eae8f0a72324b72a4c0bf50fbd5
songzhw <songzhw2012@gmail.com>
2020-01-06 16:18:30 -0500

    kotlin sam

--- Advanced_kb/src/function/sam/SamDemo.kt
+++ Advanced_kb/src/function/sam/SamDemo.kt
@@ -16,11 +16,22 @@ fun main() {
 
     // the lambda version is:
     obj.setCheckListener(ICheckListener { })
+    // obj.setCheckListener{isCheck -> println(isCheck)} // ERROR!
 
 
-
+    val checkObj = CheckDemo()
+    checkObj.setChecker { isCheck -> println(isCheck) }
+    checkObj.setChecker(ICheckListener {  })
+    checkObj.setChecker(object : ICheckListener{
+        override fun onChceck(isCheck: Boolean) {
+        }
+    })
 
     // This is wrong, as kotlin does not support kotlin's SAM
     // obj.setClickListener(IClickListener { id -> println() })
 
+    obj.setClickListener(object : IClickListener {
+        override fun onClick(id: Int) {
+        }
+    })
 }
