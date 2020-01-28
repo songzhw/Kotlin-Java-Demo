5bc6e5825a5a72def2029cb553e0d9ce1a883b45
songzhw <songzhw2012@gmail.com>
2020-01-28 14:33:25 -0500

    customize the runner

--- Advanced_kb/test/ExecutionTimeRunner.kt
+++ Advanced_kb/test/ExecutionTimeRunner.kt
@@ -1,8 +1,17 @@
 import org.junit.runners.BlockJUnit4ClassRunner
+import org.junit.runners.model.FrameworkMethod
+import org.junit.runners.model.Statement
 
-class ExecutionTimeRunner: BlockJUnit4ClassRunner{
-    constructor(clazz: Class<*>){
-        super(class)
+class ExecutionTimeRunner(clazz: Class<*>) : BlockJUnit4ClassRunner(clazz) {
 
+    override fun withBefores(method: FrameworkMethod, target: Any, base: Statement): Statement {
+        val junitStatement = super.withBefores(method, target, base)
+        return object : Statement() {
+            override fun evaluate() {
+                println("szw 自定义Runner.Before")
+                junitStatement.evaluate()
+            }
+        }
     }
+
 }
