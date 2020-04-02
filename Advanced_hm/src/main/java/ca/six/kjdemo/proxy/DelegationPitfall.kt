3ddcfdd5af4846d91f3803d84e2703d0a6900ab9
songzhw <songzhw2012@gmail.com>
2020-04-02 12:07:34 -0400

    delegation

--- Advanced_hm/src/main/java/ca/six/kjdemo/proxy/DelegationPitfall.kt
+++ Advanced_hm/src/main/java/ca/six/kjdemo/proxy/DelegationPitfall.kt
@@ -2,19 +2,29 @@ package ca.six.kjdemo.proxy
 
 interface IDelegatePitfall {
     val id: String
+    val name: String
     fun print()
+    fun foo();
 }
 
 class DelegationPitfallImpl : IDelegatePitfall {
     override val id = "biz"
-    override fun print() { print(id) }
+    override val name ="bizName"
+    override fun print() { println(id) }
+    override fun foo() { println(id) }
 }
 
 class Wrapper(biz: IDelegatePitfall) : IDelegatePitfall by biz{
     override val id = "wrapper"
+
+    override fun foo() {
+        println(id)
+    }
 }
 
 fun main() {
     val obj = Wrapper(DelegationPitfallImpl())
-    obj.print() //=> biz (注意,这里不是wrapper!!!)
+    obj.print() //=> biz (注意,这里不是wrapper!!!  因为wrapper没有覆写print, 所以print里调用的仍是biz里的id对象)
+    println(obj.name) //=> bizName
+    obj.foo()   //=> wrapper (因为wrapper自己override了foo, 所以是使用自己的member)
 }
