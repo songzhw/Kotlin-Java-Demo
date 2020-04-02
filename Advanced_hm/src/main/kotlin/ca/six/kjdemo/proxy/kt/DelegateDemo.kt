bba22838ab3e997686abffcae593aaeb75f7a87f
songzhw <songzhw2012@gmail.com>
2020-04-02 11:43:52 -0400

    kotlin delegation - Decorator Pattern

--- Advanced_hm/src/main/kotlin/ca/six/kjdemo/proxy/kt/DelegateDemo.kt
+++ Advanced_hm/src/main/kotlin/ca/six/kjdemo/proxy/kt/DelegateDemo.kt
@@ -15,7 +15,7 @@ class FetchDataWorkJob : IWorkJob {
     }
 }
 
-class LogWrapper(val bizJob: IWorkJob): IWorkJob by bizJob {
+class LogWrapper(val bizJob: IWorkJob) : IWorkJob by bizJob {
     override fun work() {
         println("=== before log === (${System.currentTimeMillis()})")
         bizJob.work()
@@ -23,16 +23,40 @@ class LogWrapper(val bizJob: IWorkJob): IWorkJob by bizJob {
     }
 }
 
+class TimeWrapper(val bizJob: IWorkJob) : IWorkJob by bizJob {
+    override fun work() {
+        val start = System.currentTimeMillis()
+        bizJob.work()
+        val end = System.currentTimeMillis()
+        println("TimeWrapper : consume time = ${end - start}ms")
+    }
+}
 
 fun main() {
     val fetcher = FetchDataWorkJob()
     val worker = LogWrapper(fetcher)
     worker.work()
     worker.despcription()
+
+    println("\n******************************\n")
+
+    val fetcher2 = FetchDataWorkJob()
+    val worker2 = TimeWrapper(LogWrapper(fetcher2))
+
+    worker2.work()
+    worker2.despcription()
+
 }
 /*
 === before log === (1585841097667)
 http working...
 === after log === (1585841097667)
+I'm fetch
+
+
+=== before log === (1585842176441)
+http working...
+=== after log === (1585842176441)
+TimeWrapper : consume time = 0ms
 I'm fetch
  */
