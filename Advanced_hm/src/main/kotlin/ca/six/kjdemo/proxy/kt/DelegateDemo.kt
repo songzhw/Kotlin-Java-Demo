e1c0897d4e715b85e24b87207a643a4818cc4ad5
songzhw <songzhw2012@gmail.com>
2020-04-02 11:25:22 -0400

    kotlin delegation - use bizJob for un-override method

--- Advanced_hm/src/main/kotlin/ca/six/kjdemo/proxy/kt/DelegateDemo.kt
+++ Advanced_hm/src/main/kotlin/ca/six/kjdemo/proxy/kt/DelegateDemo.kt
@@ -2,25 +2,37 @@ package ca.six.kjdemo.proxy.kt
 
 interface IWorkJob {
     fun work()
+    fun despcription()
 }
 
 class FetchDataWorkJob : IWorkJob {
     override fun work() {
         println("http working...")
     }
+
+    override fun despcription() {
+        println("I'm fetch")
+    }
 }
 
-class LogWorkJob(val bizJob: IWorkJob): IWorkJob by bizJob {
+class LogWrapper(val bizJob: IWorkJob): IWorkJob by bizJob {
     override fun work() {
         println("=== before log === (${System.currentTimeMillis()})")
         bizJob.work()
         println("=== after log === (${System.currentTimeMillis()})")
     }
 }
-//TODO another wrapper
+
 
 fun main() {
     val fetcher = FetchDataWorkJob()
-    val worker = LogWorkJob(fetcher)
+    val worker = LogWrapper(fetcher)
     worker.work()
+    worker.despcription()
 }
+/*
+=== before log === (1585841097667)
+http working...
+=== after log === (1585841097667)
+I'm fetch
+ */
