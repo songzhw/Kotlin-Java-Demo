a8e552fccf96234d25a81813acaf1db04113f137
songzhw <songzhw2012@gmail.com>
2020-04-03 10:31:30 -0400

    kotlin prop delegate

--- Advanced_hm/src/main/kotlin/ca/six/kjdemo/proxy/kt/PrpsDelegationUsage1.kt
+++ Advanced_hm/src/main/kotlin/ca/six/kjdemo/proxy/kt/PrpsDelegationUsage1.kt
@@ -6,7 +6,7 @@ import kotlin.reflect.KProperty
 fun Int.coerceIn(max: Int): Int {
     var ret = this
     if (this > max) {
-        ret = max % this;
+        ret = this % max;
     }
     return ret
 }
@@ -23,12 +23,19 @@ var minute1 = 49
 
 
 // ========== 使用delegate  ==========
-class MaxDelegate(var value: Int, val max: Int) : ReadWriteProperty<Int, Int> {
-    override fun getValue(thisRef: Int, property: KProperty<*>): Int = value
+class MaxDelegate(var value: Int, val max: Int) : ReadWriteProperty<Any?, Int> {
+    override fun getValue(thisRef: Any?, property: KProperty<*>): Int = value
 
-    override fun setValue(thisRef: Int, property: KProperty<*>, value: Int) {
+    override fun setValue(thisRef: Any?, property: KProperty<*>, value: Int) {
         this.value = value.coerceIn(max)
     }
 }
 
-var hour2 by MaxDelegate(10, 60)
+var hour2 by MaxDelegate(10, 24)
+val minute2 by MaxDelegate(33, 60)
+
+
+fun main() {
+    hour2 = 28
+    println(hour2) //=> 4
+}
