832e62b936c1f5970557e450a42d3535eb0a3446
songzhw <songzhw2012@gmail.com>
2020-05-15 10:43:28 -0400

    fix the odd issue

--- Advanced_hm/src/main/kotlin/ca/six/kjdemo/temp.kt
+++ Advanced_hm/src/main/kotlin/ca/six/kjdemo/temp.kt
@@ -5,10 +5,13 @@ class Item
 
 fun main() {
     val map1 = mapOf(String::class.java to "first", Int::class.java to "second")
-    foo(map1)
+    // foo(map1) // => Error
 
     val map2 = mapOf(String::class.java to "first", Item::class.java to "second")
     foo(map2)
+
+    val map3: Map<Class<out Any>, String> = mapOf(String::class.java to "first", Int::class.java to "second")
+    foo(map3)
 }
 
 
