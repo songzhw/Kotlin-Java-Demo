6d9767a00984e72b9d33f9bb0ab1655fa2d9c6be
songzhw <songzhw2012@gmail.com>
2020-06-04 17:16:46 -0400

    if else

---
+++ Advanced_hm/src/main/kotlin/ca/six/kjdemo/architecture/ifelse/HashMap大法.kt
@@ -0,0 +1,37 @@
+package ca.six.kjdemo.architecture.ifelse
+
+enum class Condition11 {
+    ONE,
+    TWO,
+    THREE,
+    FOUR
+}
+
+fun original(condition: Condition11) {
+    if (condition === Condition11.ONE) {
+        println("001")
+    } else if (condition === Condition11.TWO) {
+        println("002")
+    } else if (condition === Condition11.THREE) {
+        println("003")
+    } else {
+        println("004")
+    }
+}
+
+fun refactor11(condition: Condition11) {
+    val actions = HashMap<Condition11, () -> Unit>()
+    // actions.put(Condition11.ONE, ()->{ println("001"); }) //ERROR
+    actions.put(Condition11.ONE, { println("001"); })
+    actions.put(Condition11.TWO, { println("002"); })
+    actions.put(Condition11.THREE, { println("003"); })
+
+    actions[condition]?.invoke()
+}
+
+
+fun main() {
+    original(Condition11.FOUR)
+
+    refactor11(Condition11.FOUR)
+}
