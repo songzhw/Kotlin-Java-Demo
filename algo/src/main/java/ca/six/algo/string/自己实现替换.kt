6fc0b3fc9bdcd18f14a1aa7277bc118832fd90f9
songzhw <songzhw2012@gmail.com>
2020-06-05 09:53:41 -0400

    string replace

---
+++ algo/src/main/java/ca/six/algo/string/自己实现替换.kt
@@ -0,0 +1,22 @@
+package ca.six.algo.string
+
+fun originalReplaceWhitespace(str: Array<Char>) {
+//    for (char in str) {
+//        if (char == ' ') {
+//            // index
+//            for(i = index+2; index <= length + 2; index++){
+//                str[i] = str[index]
+//            }
+//        }
+//    }
+}
+
+fun replaceWhitespace(str: CharArray) {
+
+
+
+}
+
+fun main() {
+    replaceWhitespace("welcome to here".toCharArray())
+}
