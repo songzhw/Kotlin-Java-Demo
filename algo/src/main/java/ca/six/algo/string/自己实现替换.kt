6bdc4c9902b2f8ff406104926b2f3440d8c5f2be
songzhw <songzhw2012@gmail.com>
2020-06-05 10:01:06 -0400

    to fix an ArrayOutBoundary error

--- algo/src/main/java/ca/six/algo/string/自己实现替换.kt
+++ algo/src/main/java/ca/six/algo/string/自己实现替换.kt
@@ -12,7 +12,28 @@ fun originalReplaceWhitespace(str: Array<Char>) {
 }
 
 fun replaceWhitespace(str: CharArray) {
+    var spaceCount = 0
+    for(char in str){
+        if(char == ' ') spaceCount++
+    }
 
+    val sizeBefore = str.size
+    val sizeAfter = sizeBefore + spaceCount * 2 // " "要换成"%20", 即每个空格的长度增加了2
+    val p1 = sizeBefore - 1
+    val p2 = sizeAfter - 2
+
+    // 原来的str长度写死了, 不另写一个ret, 就会越界的!
+    val ret = CharArray(sizeAfter)
+    var index = 0
+//    for(char in str){
+//        ret.set(index, char)
+//        index++
+//    }
+
+
+
+
+    println(ret)
 
 
 }
