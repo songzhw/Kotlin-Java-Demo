f870852bca0a49354c50c7c3a20ab03f8855ad77
songzhw <songzhw2012@gmail.com>
2020-06-09 09:38:36 -0400

    StackOerflow Error

--- algo/src/main/java/ca/six/algo/recurse/Fibonacci数列.kt
+++ algo/src/main/java/ca/six/algo/recurse/Fibonacci数列.kt
@@ -1,13 +1,14 @@
 package ca.six.algo.recurse
 
 fun fibonacci(n: Int): Int {
+    println(n)
     if (n == 0) return 0
     if (n == 1) return 1
     else return fibonacci(n - 1) + fibonacci(n - 2)
 }
 
 fun main() {
-    println("fibonnaci : ${fibonacci(4)}")
-    println("fibonnaci : ${fibonacci(5)}")
-    println("fibonnaci : ${fibonacci(6)}")
+//    println("fibonnaci : ${fibonacci(4)}")
+//    println("fibonnaci : ${fibonacci(5)}")
+    println("fibonnaci : ${fibonacci(66666)}")
 }
