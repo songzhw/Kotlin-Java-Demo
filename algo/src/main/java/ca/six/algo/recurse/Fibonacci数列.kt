0a51b4f28e5e05298984e56c5e8ffa3696349ab0
songzhw <songzhw2012@gmail.com>
2020-06-09 10:27:50 -0400

    good performance

--- algo/src/main/java/ca/six/algo/recurse/Fibonacci数列.kt
+++ algo/src/main/java/ca/six/algo/recurse/Fibonacci数列.kt
@@ -6,13 +6,43 @@ fun bad_recursive_fibonacci(n: Int): Int {
     else return bad_recursive_fibonacci(n - 1) + bad_recursive_fibonacci(n - 2)
 }
 
+// = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+
+val map = HashMap<Int, Int>()
+
+fun good_recursive_fibonacci(n: Int): Int {
+    if (n == 0) map.put(0, 0)
+    else if (n == 1) map.put(1, 1)
+    else {
+        var minus2 = map.get(n - 2)
+        var minus1 = map.get(n - 1)
+        //println("($n): minus1 = $minus1, minus2 = $minus2")
+        if (minus1 == null) {
+            minus1 = good_recursive_fibonacci(n - 1)
+            map.set(n - 1, minus1)
+            //println("   =+> (${n-1}) = $minus1")
+        }
+        if (minus2 == null) {
+            minus2 = good_recursive_fibonacci(n - 2)
+            map.set(n - 2, minus2)
+            //println("   =*> (${n-2}) = $minus2")
+        }
+        map.put(n, minus1 + minus2)
+    }
+    return map.get(n)!!
+}
+
+
+// = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+
 fun main() {
 //    println("fibonnaci : ${fibonacci(4)}")
 //    println("fibonnaci : ${fibonacci(5)}")
     val start = System.currentTimeMillis()
-    println("fibonnaci : ${bad_recursive_fibonacci(50)}")
+    println("fibonnaci : ${good_recursive_fibonacci(50)}")
     val end = System.currentTimeMillis()
     println("cost time = ${(end - start) / 1000f}")
 }
 
-// f(30)耗时0.006秒; f(40)耗时0.413秒; f(50)是48.397秒; f(100)执行了20多分钟都没有结束; (其实f(60)就已经等了好久都没有结果
+// bad_recursive_fibonacci(30)耗时0.006秒; bad_recursive_fibonacci(40)耗时0.413秒; bad_recursive_fibonacci(50)是48.397秒; bad_recursive_fibonacci(100)执行了20多分钟都没有结束; (其实bad_recursive_fibonacci(60)就已经等了好久都没有结果
+// good_recursive_fibonacci(50)耗时0.005秒; good_recursive_fibonacci(150)耗时0.013秒 || 当然good_recursive_fibonacci(66666)仍会有StackOverflowError (因为这仍是在使用递归)
