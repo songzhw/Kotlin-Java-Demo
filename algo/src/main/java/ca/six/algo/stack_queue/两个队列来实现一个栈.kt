2b6c06ab281f2823b402f3d31e19042cedaa49e6
songzhw <songzhw2012@gmail.com>
2020-06-09 09:51:31 -0400

    java call kotlin

--- algo/src/main/java/ca/six/algo/stack_queue/两个队列来实现一个栈.kt
+++ algo/src/main/java/ca/six/algo/stack_queue/两个队列来实现一个栈.kt
@@ -4,5 +4,29 @@ import java.util.*
 
 class Q2Stack<E> {
 //    private val inQueue = Queue<E>() // Queue在java中是接口, 不能new
+    private val inQueue = LinkedList<E>() // LinkedList是Queue接口的实现类
+    private val outQueue = LinkedList<E>()
+
+    fun push(e: E){
+
+    }
+
+    fun pop(){}
+
+}
+
+fun main() {
+    val stack = Q2Stack<Int>()
+    stack.push(1)
+    stack.push(2)
+    stack.push(3)
+
+    println(stack.pop())
+
+    stack.push(4)
+    println(stack.pop())
+    println(stack.pop())
+    println(stack.pop())
+
 
 }
