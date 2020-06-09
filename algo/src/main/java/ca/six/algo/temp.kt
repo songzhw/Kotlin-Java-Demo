4153e1820cf829dd26e7093dbd983bbd1d0ab55c
songzhw <songzhw2012@gmail.com>
2020-06-09 10:21:10 -0400

    good performance

--- algo/src/main/java/ca/six/algo/temp.kt
+++ algo/src/main/java/ca/six/algo/temp.kt
@@ -9,15 +9,17 @@ fun f(n: Int): Int {
     else {
         var minus2 = map.get(n - 2)
         var minus1 = map.get(n - 1)
-        println("($n): minus1 = $minus1, minus2 = $minus2")
+        //println("($n): minus1 = $minus1, minus2 = $minus2")
         if (minus1 == null) {
             minus1 = f(n - 1)
-            map.set(minus1, minus1)
-        } else if (minus2 == null) {
-            minus2 = f(n - 2)
-            map.set(minus2, minus2)
+            map.set(n - 1, minus1)
+            //println("   =+> (${n-1}) = $minus1")
         }
-        println("    => ($n): minus1 = $minus1, minus2 = $minus2")
+        if (minus2 == null) {
+            minus2 = f(n - 2)
+            map.set(n - 2, minus2)
+            //println("   =*> (${n-2}) = $minus2")
+        }
         map.put(n, minus1 + minus2!!)
     }
     return map.get(n)!!
@@ -25,7 +27,10 @@ fun f(n: Int): Int {
 
 fun main() {
     val start = System.currentTimeMillis()
-    println(f(2))
+    // println(f(66666)) //=> java.lang.StackOverflowError
+    println(f(150))
     val end = System.currentTimeMillis()
     println("cost time2 = ${(end - start) / 1000f}")
 }
+// f(50)耗时0.005秒;  f(150)耗时0.013秒
+// 当然f(66666)仍会有StackOverflowError (因为这仍是在使用递归)
