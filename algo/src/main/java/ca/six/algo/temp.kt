1c69f39665646663cd7ed1bb50ed90a7046d6cca
songzhw <songzhw2012@gmail.com>
2020-06-11 09:52:16 -0400

    power

--- algo/src/main/java/ca/six/algo/temp.kt
+++ algo/src/main/java/ca/six/algo/temp.kt
@@ -1,1 +1,14 @@
 package ca.six.algo
+
+fun power(base: Double, exponent: Int) {
+    var ret = base
+    for (i in 2..exponent) {
+        ret *= base
+    }
+    println("power($base, $exponent) = $ret")
+}
+
+fun main() {
+    power(3.0, 3) //=> 27
+    power(2.0, 4) //=> 16
+}
