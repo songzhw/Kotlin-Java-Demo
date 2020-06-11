ac3604dcbbc16b8fdfeef745a163076c5e5aef53
songzhw <songzhw2012@gmail.com>
2020-06-11 09:56:02 -0400

    边界过大, 如1000时, 数字要转为long

--- algo/src/main/java/ca/six/algo/temp.kt
+++ algo/src/main/java/ca/six/algo/temp.kt
@@ -1,19 +1,27 @@
 package ca.six.algo
 
 fun power(base: Double, exponent: Int) {
-    var ret = base
+    if(exponent < 0) {
+        println("exponent can't be negative")
+        return
+    }
+
+    var ret = base.toLong()
     for (i in 2..exponent) {
-        ret *= base
+        ret *= base.toLong()
     }
     println("power($base, $exponent) = $ret")
 }
 
 fun main() {
     // 1. 功能性测试
-    power(3.0, 3) //=> 27
-    power(2.0, 4) //=> 16
+    power(3.0, 3)  //=> 27
+    power(2.0, 4)  //=> 16
 
     // 2. 边界测试
-    power(3.0, 1) //=> 3
-    power(3.0, -2)
+    power(3.0, 1)  //=> 3
+    power(3.0, -2) //=> exponent can't be negative
+
+    // 3. 性能测试
+    power(3.0, 1000) //=> 6203307696791771937
 }
