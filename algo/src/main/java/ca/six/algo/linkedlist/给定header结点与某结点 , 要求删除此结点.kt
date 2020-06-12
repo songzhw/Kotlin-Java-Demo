64dbabd41b6af667b82ba0a228c16860347397da
songzhw <songzhw2012@gmail.com>
2020-06-12 09:12:14 -0400

    okay

--- algo/src/main/java/ca/six/algo/linkedlist/给定header结点与某结点 , 要求删除此结点.kt
+++ algo/src/main/java/ca/six/algo/linkedlist/给定header结点与某结点 , 要求删除此结点.kt
@@ -3,17 +3,29 @@ package ca.six.algo.linkedlist
 // 给定Header与toBeDeleted结点, 要求删除toBeDeleted结点
 fun deleteNode(header: NodeC, toBeDeleted: NodeC) {
     val next1 = toBeDeleted.next
-    val next2 = next1!!.next
 
-    toBeDeleted.data = next1.data
-    toBeDeleted.next = next2
+    // 说明toBeDeleted已经是尾结点了
+    if (next1 == null) {
+        deleteNodeByIteration(toBeDeleted)
+    } else {
+        val next2 = next1.next
+
+        toBeDeleted.data = next1.data
+        toBeDeleted.next = next2
+    }
 
     var temp: NodeC? = header
     while (temp != null) {
         print("${temp.data}, ")
         temp = temp.next
     }
+    println("")
 }
+
+fun deleteNodeByIteration(node: NodeC){
+    // TODO 一个普通的遍历
+}
+
 
 fun main() {
     val node5 = NodeC(5)
