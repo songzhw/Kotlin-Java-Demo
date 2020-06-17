0f83795d38963bcd8f18be15fcdcfb619a17e654
songzhw <songzhw2012@gmail.com>
2020-06-17 07:50:40 -0400

    error - kotlin array is not expandable

---
+++ algo/src/main/java/ca/six/algo/tree/sku全排列.kt
@@ -0,0 +1,22 @@
+package ca.six.algo.tree
+
+import java.util.*
+
+val color = arrayOf("黑", "白")
+val size = arrayOf("10", "8")
+val ram = arrayOf("64", "256")
+
+fun allSku(vararg skus: Array<String>) {
+    val results = arrayListOf<String>()
+    val stack = Stack<String>()
+}
+
+fun main() {
+    val results: Array<String> = emptyArray()
+    results[0]="c"
+    results[1]="a"
+    results[2]="b"
+    println(results)
+}
+
+
