ecdd22e8f49072c6a06a936c61b389fd31d05c98
songzhw <songzhw2012@gmail.com>
2020-06-18 09:11:33 -0400

    lru cache 1

--- algo/src/main/java/ca/six/algo/biz/LruCache.kt
+++ algo/src/main/java/ca/six/algo/biz/LruCache.kt
@@ -1,11 +1,24 @@
 package ca.six.algo.biz
 
-class LruCache(val size: Int) {
-    fun put(key: String, value: Int){
+import java.util.*
+import kotlin.collections.HashMap
 
+class LruCache(val size: Int) {
+    // map结构保证put与get全是O(1)
+    private val map = HashMap<String, Int>()
+
+    // double linked list用来保存"顺序". 双链表(而非单链表)是为了保证删除时时间为O(1)
+    private val list = LinkedList<String>()
+
+    fun put(key: String, value: Int) {
+        // TODO what if key exists already
+        map.put(key, value)
+        list.add(key)
     }
 
-    fun get(key:String){
+    fun get(key: String) :Int? {
+        // TODO what if the key is not existing?
+        return map.get(key)
     }
 }
 
