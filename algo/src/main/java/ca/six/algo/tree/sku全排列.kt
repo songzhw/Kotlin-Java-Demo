d33348f9c6755bbdeabafe43249cc1862731ed31
songzhw <songzhw2012@gmail.com>
2020-06-19 08:50:36 -0400

    + is the answer

--- algo/src/main/java/ca/six/algo/tree/sku全排列.kt
+++ algo/src/main/java/ca/six/algo/tree/sku全排列.kt
@@ -7,23 +7,23 @@ val size = arrayOf("10'", "8'")
 val ram = arrayOf("64G", "256G")
 
 fun allSku(result: ArrayList<ArrayList<String>>, vararg skuList: Array<String>) {
-    loop(ArrayList<String>(), 0, *skuList);
+    loop(arrayOf<String>(), 0, *skuList);
 }
 
-fun loop(prevWork: ArrayList<String>, skuIndex: Int, vararg skuList: Array<String>) {
+fun loop(prevWork: Array<String>, skuIndex: Int, vararg skuList: Array<String>) {
     val currentSkus = skuList[skuIndex]
     val isLastSkus = skuIndex == (skuList.size - 1)
     for(sku in currentSkus){
-//        val work = prevWork.toArray().concat
+        val work = prevWork + sku
     }
 }
 
 fun main() {
     val result = ArrayList<ArrayList<String>>()
-    allSku(result, color, size)
+//    allSku(result, color, size)
 
-    val ret = color+ size
-    println(ret)
+
+    // println( (color + "ru").asList())  //=> [黑, 白, ru]
 
 //    for(index in 10 downTo 7) print(index)  //=> 10, 9, 8, 7
 }
