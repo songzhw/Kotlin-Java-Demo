ee574371dac71a79b1526ef2336e3cd729a89b77
songzhw <songzhw2012@gmail.com>
2020-06-19 08:54:16 -0400

    this seems work

--- algo/src/main/java/ca/six/algo/tree/sku全排列.kt
+++ algo/src/main/java/ca/six/algo/tree/sku全排列.kt
@@ -6,21 +6,30 @@ val color = arrayOf("黑", "白")
 val size = arrayOf("10'", "8'")
 val ram = arrayOf("64G", "256G")
 
-fun allSku(result: ArrayList<ArrayList<String>>, vararg skuList: Array<String>) {
-    loop(arrayOf<String>(), 0, *skuList);
+fun allSku(result: ArrayList<Array<String>>, vararg skuList: Array<String>) {
+    loop(arrayOf<String>(), 0, result, *skuList);
+
+    for(ary in result){
+        println(ary.asList())
+    }
 }
 
-fun loop(prevWork: Array<String>, skuIndex: Int, vararg skuList: Array<String>) {
+fun loop(prevWork: Array<String>, skuIndex: Int, result: ArrayList<Array<String>>, vararg skuList: Array<String>) {
     val currentSkus = skuList[skuIndex]
     val isLastSkus = skuIndex == (skuList.size - 1)
-    for(sku in currentSkus){
+    for (sku in currentSkus) {
         val work = prevWork + sku
+        if (isLastSkus) result.add(work)
+        else {
+            val newIndex = skuIndex + 1
+            loop(work, newIndex, result, *skuList)
+        }
     }
 }
 
 fun main() {
-    val result = ArrayList<ArrayList<String>>()
-//    allSku(result, color, size)
+    val result = ArrayList<Array<String>>()
+    allSku(result, color, size)
 
 
     // println( (color + "ru").asList())  //=> [黑, 白, ru]
