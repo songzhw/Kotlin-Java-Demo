eb9aa2fb8b4c15386d02c5e1232dc65fc5f6e616
songzhw <songzhw2012@gmail.com>
2020-06-19 09:01:57 -0400

    kotlin不支持函数中调用函数吗?(这代码会报错)

--- algo/src/main/java/ca/six/algo/tree/sku全排列.kt
+++ algo/src/main/java/ca/six/algo/tree/sku全排列.kt
@@ -2,30 +2,35 @@ package ca.six.algo.tree
 
 import java.util.*
 
+// 1. 函数中函数, 改为不嵌套了
+// 2. ary.concat(ary2), 在kotlin中可以用+来代替
+
 val color = arrayOf("黑", "白", "金")
 val size = arrayOf("10'", "8'")
 val ram = arrayOf("64G", "256G", "512G")
 
 fun allSku(result: ArrayList<Array<String>>, vararg skuList: Array<String>) {
-    loop(arrayOf<String>(), 0, result, *skuList);
+    loop(arrayOf<String>(), 0);
+
+    fun loop(prevWork: Array<String>, skuIndex: Int) {
+        val currentSkus = skuList[skuIndex]
+        val isLastSkus = skuIndex == (skuList.size - 1)
+        for (sku in currentSkus) {
+            val work = prevWork + sku
+            if (isLastSkus) result.add(work)
+            else {
+                val newIndex = skuIndex + 1
+                loop(work, newIndex, result, *skuList)
+            }
+        }
+    }
 
     for(ary in result){
         println(ary.asList())
     }
 }
 
-fun loop(prevWork: Array<String>, skuIndex: Int, result: ArrayList<Array<String>>, vararg skuList: Array<String>) {
-    val currentSkus = skuList[skuIndex]
-    val isLastSkus = skuIndex == (skuList.size - 1)
-    for (sku in currentSkus) {
-        val work = prevWork + sku
-        if (isLastSkus) result.add(work)
-        else {
-            val newIndex = skuIndex + 1
-            loop(work, newIndex, result, *skuList)
-        }
-    }
-}
+
 
 fun main() {
     val result = ArrayList<Array<String>>()
