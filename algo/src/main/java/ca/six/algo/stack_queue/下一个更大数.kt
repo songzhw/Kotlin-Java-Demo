2631e5acc886e2b66cee3e4f0b54b0aee2c6365b
songzhw <songzhw2012@gmail.com>
2020-06-21 13:20:00 -0400

    stack的peek()竟然会在为空时抛异常         if (len == 0)             throw new EmptyStackException();

--- algo/src/main/java/ca/six/algo/stack_queue/下一个更大数.kt
+++ algo/src/main/java/ca/six/algo/stack_queue/下一个更大数.kt
@@ -1,5 +1,7 @@
 package ca.six.algo.stack_queue
 
+import java.util.*
+
 /*
 如输入是[2,1,2,4,3], 要求返回每个元素后面第一个更大的数 (若没有, 就返回-1)
 2后面第一个比2更大的数是4; 1后面第一个比1更大的数是2; ...
@@ -8,6 +10,19 @@ package ca.six.algo.stack_queue
 
 fun nextGreater(src: Array<Int>) {
     val ret = arrayOfNulls<Int>(src.size)
+    val stack = Stack<Int>()
+
+    //是往后的更大数, 所以使用倒序来遍历src
+    for (i in src.size - 1 downTo 0) { //10 downto 8是: 10, 9, 8
+        val number = src[i]
+        println(stack)
+        if (!stack.isEmpty() and (stack.peek() <= number)) {
+            stack.pop()
+        }
+        ret[i] = if (stack.empty()) -1 else stack.pop()
+        stack.push(number)
+    }
+
     println(ret.asList())
 }
 
