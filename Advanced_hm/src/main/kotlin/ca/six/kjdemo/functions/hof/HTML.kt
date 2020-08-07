27040fd53c91ad79f6f795d9e2ec24b9e6d3755c
songzhw <songzhw2012@gmail.com>
2020-08-07 17:54:10 -0400

    receiver func

--- Advanced_hm/src/main/kotlin/ca/six/kjdemo/functions/hof/HTML.kt
+++ Advanced_hm/src/main/kotlin/ca/six/kjdemo/functions/hof/HTML.kt
@@ -1,5 +1,30 @@
 package ca.six.kjdemo.functions.hof
 
-class HTML: BaseHtmlElement("html") {
 
+class P(content: String) : BaseHtmlElement("p", content)
+class H1(content: String) : BaseHtmlElement("h1", content)
+
+class Body : BaseHtmlElement("body") {
+    fun h1(block: () -> String): H1 {
+        val content = block()
+        val h1 = H1(content)
+        this.children += h1
+        return h1
+    }
+
+    fun p(block: () -> String): P {
+        val content = block()
+        val p = P(content)
+        this.children += p
+        return p
+    }
+}
+
+class HTML : BaseHtmlElement("html") {
+    fun body(block: () -> String): H1 {
+        val content = block()
+        val h1 = H1(content)
+        this.children += h1
+        return h1
+    }
 }
