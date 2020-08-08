6505273e391fce6906460ab076c8f137f8f28ad9
songzhw <songzhw2012@gmail.com>
2020-08-07 20:59:38 -0400

    good

--- Advanced_hm/src/main/kotlin/ca/six/kjdemo/temp.kt
+++ Advanced_hm/src/main/kotlin/ca/six/kjdemo/temp.kt
@@ -1,7 +1,33 @@
 package ca.six.kjdemo
 
-import kotlin.math.sign
+
+val sum1: Int.(Int) -> Unit = { other -> plus(other) }
+val sum2: Int.(Int) -> Int = { other -> plus(other) }
+
+class FuncReceiver {
+    fun f1(value: Int): String {
+        return "str$value"
+    }
+
+    fun f2(arg: String): Int {
+        return arg.hashCode()
+    }
+
+    fun f3(): Boolean {
+        return false
+    }
+
+    fun f4() {}
+}
+
+fun r1(work: ()->Unit) {
+    val ret = work()
+    println("r1 = $ret")
+}
+
+
 
 fun main() {
-    println("now = ${System.currentTimeMillis()}")
+    println(2.sum1(3))  //=> kotlin.Unit
+    println(2.sum2(3))  //=> 5
 }
