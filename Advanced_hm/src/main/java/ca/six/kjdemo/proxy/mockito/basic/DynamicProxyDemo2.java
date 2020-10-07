01492118b0bee8a7adbad6f913324d75bfe63804
songzhw <songzhw2012@gmail.com>
2020-10-07 16:39:22 -0400

    dynamic proxy

---
+++ Advanced_hm/src/main/java/ca/six/kjdemo/proxy/mockito/basic/DynamicProxyDemo2.java
@@ -0,0 +1,55 @@
+package ca.six.kjdemo.proxy.mockito.basic;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+
+interface IGreet {
+    void sayHello(String name);
+}
+
+
+class GreetImpl implements IGreet {
+    @Override
+    public void sayHello(String name) {
+        System.out.println("Hello " + name);
+    }
+}
+
+// 原文都是使用IGreet的对象, 实为GreetImpl的对象在做事. 即IGreet obj = new GreetImpl();
+// 现在要修改一些逻辑, 但不准/不能改动原有的GreetImpl. 这要怎么做?  (是的, AOP就是动态代码的应用实例之一)
+//    • 方法一: 静态代理. 写一个Greet2类, 内有成员GrretImplements, 也实现IGreet.
+//    • 方法二: 动态代理. 如下面的代码
+
+// 注意, 它不是实现IGreet接口. 它是实现的InvocationHandler接口, 要实现invoke()方法
+class Greet3 implements InvocationHandler {
+    private Object delegate;
+
+    public Object bind(Object delegate) {
+        this.delegate = delegate;
+
+        return Proxy.newProxyInstance(
+                delegate.getClass().getClassLoader(),
+                delegate.getClass().getInterfaces(),
+                this
+        );
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        System.out.println("insert new logic here");
+        Object result = method.invoke(this.delegate, args);
+        return result;
+    }
+}
+
+
+class DynamicProxyDemo2 {
+    public static void main(String[] args) {
+        IGreet worker = new GreetImpl();
+        IGreet obj = (IGreet) new Greet3().bind(worker);
+        obj.sayHello("111");
+    }
+}
+
--- Advanced_hm/src/main/kotlin/ca/six/kjdemo/temp.kt
+++ Advanced_hm/src/main/kotlin/ca/six/kjdemo/temp.kt
@@ -5,18 +5,13 @@ import kotlin.experimental.xor
 
 fun main() {
     val MOD = 20
-//    val data = "ObfuscatedFonts123".toByteArray()
-//    val data = byteArrayOf(10, 20, 30, 40)
-    val data = byteArrayOf(13, 18, 27, 44)
-    val key = byteArrayOf(7, 6, 5, 4, 3, 2, 1)
+//    val data = byteArrayOf(10, 20, 30, 40, 50, 60, 70, 80, 90)
+    val data = byteArrayOf(10, 29, 22, 47, 53, 58, 67, 84, 89)
+    val key = byteArrayOf(0, 9, 8, 7, 7, 6, 5, 4, 3, 2, 1)
     val size = data.size
     for (i in 0 until size) {
         data[i] = data[i] xor key[i % MOD]
     }
 
-//    print(Base64.getEncoder().encodeToString(data))
-//    print(String(data))
-    data.forEach { print("$it,") }
+    print(data.joinToString(","))
 }
-
-//=> AAAAAAAAAAAMCygkCw1KCQUF
