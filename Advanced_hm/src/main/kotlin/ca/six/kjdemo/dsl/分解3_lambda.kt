e351d1f4c757e2296df0bd5e74428a7c3c23eaee
songzhw <songzhw2012@gmail.com>
2020-11-03 15:34:46 -0500

    receiver

--- Advanced_hm/src/main/kotlin/ca/six/kjdemo/dsl/分解3_lambda.kt
+++ Advanced_hm/src/main/kotlin/ca/six/kjdemo/dsl/分解3_lambda.kt
@@ -8,9 +8,41 @@ fun foo4(myblock: StringBuilder.()->Unit){
     myblock(StringBuilder())
 }
 
+
+class FuncReceiver {
+    fun f1(value: Int): String {
+        return "str$value"
+    }
+
+    fun f4() {
+        println("f4")
+    }
+}
+
+fun work(){
+    println("work")
+}
+
+
+fun r1(work: FuncReceiver.()->Unit) {
+    val obj = FuncReceiver()
+    val ret = obj.work()
+    println("r1 = $ret")
+}
+
+
 fun main() {
     foo4 {
         append("001")
         print(this) //=> szw001
     }
+
+    val ret1 = r1 {
+        work()
+        println("r1 inside")
+        println(f1(33))
+        f4()
+        3
+    }
+    println("main ret = $ret1")
 }
