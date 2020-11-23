6ad3349185128a80e7e46284d4db58da74ba5fcb
songzhw <songzhw2012@gmail.com>
2020-11-23 15:24:02 -0500

    add dsl

--- Advanced_hm/src/main/kotlin/ca/six/kjdemo/dsl/fsm/FsmDemo.kt
+++ Advanced_hm/src/main/kotlin/ca/six/kjdemo/dsl/fsm/FsmDemo.kt
@@ -4,9 +4,9 @@ object Solid : State()
 object Liquid : State()
 object Gas : State()
 
-object freeze : Event()
+object Freeze : Event()
 object Melt : Event()
-object vaporize : Event()
+object Vaporize : Event()
 object Condense : Event()
 
 fun main() {
@@ -15,6 +15,10 @@ fun main() {
         state(Gas) {
             on(Condense, dest = Liquid)
         }
+        state(Liquid) {
+            on(Freeze, dest = Solid)
+            on(Vaporize, dest = Gas)
+        }
     }
 }
 
@@ -22,23 +26,29 @@ open class State
 open class Event
 
 class StateBuilder {
+    val stateMap = HashMap<Event, State>()
     fun on(event: Event, dest: State) {
-
+        stateMap[event] = dest
     }
 }
 
 class MachineBuilder {
     lateinit var state: State
+    val stateMap = HashMap<State, HashMap<Event, State>>()
 
     fun initState(state: State) {
         this.state = state
     }
 
     fun state(state: State, block: StateBuilder.() -> Unit) {
-
+        val stateBulder = StateBuilder()
+        block(stateBulder)
+        stateMap[state] = stateBulder.stateMap
     }
 }
 
-fun stateMachine(block: MachineBuilder.() -> Unit) {
-    block(MachineBuilder())
+fun stateMachine(block: MachineBuilder.() -> Unit): MachineBuilder {
+    val machine = MachineBuilder()
+    block(machine)
+    return machine
 }
