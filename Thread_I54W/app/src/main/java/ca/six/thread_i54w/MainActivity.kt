6c5a1657edfb7c5d6fa4f14d50381ae3f57a43b9
songzhw <songzhw2012@gmail.com>
2020-11-25 16:42:59 -0500

    refactor

--- Thread_I54W/app/src/main/java/ca/six/thread_i54w/MainActivity.kt
+++ Thread_I54W/app/src/main/java/ca/six/thread_i54w/MainActivity.kt
@@ -2,63 +2,26 @@ package ca.six.thread_i54w
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import ca.six.thread_i54w.biz.RoPrint1To100
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.concurrent.locks.Condition
 import java.util.concurrent.locks.Lock
 import java.util.concurrent.locks.ReentrantLock
+import java.util.function.Predicate
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val lock = ReentrantLock()
-        val turnOfA = lock.newCondition()
-        val turnOfB = lock.newCondition()
-        val t1 = Thread(RoA(lock, turnOfA, turnOfB))
-        val t2 = Thread(RoB(lock, turnOfA, turnOfB))
-
 
         btnMain.setOnClickListener {
-            t1.start()
-            t2.start()
         }
     }
 }
 
-class RoA(val lock: Lock, val conditionA: Condition, val conditionB: Condition) : Runnable {
-    override fun run() {
-        for (i in 1..100) {
-            if (i % 2 == 1) {
-                lock.lock();
-                try {
-                    println("RoA run $i")
-                    conditionB.signal()
-                    conditionA.await()
-                } finally {
-                    lock.unlock();
-                }
-            }
-        }
-    }
-}
 
-class RoB(val lock: Lock, val conditionA: Condition, val conditionB: Condition) : Runnable {
-    override fun run() {
-        for (i in 1..100) {
-            if (i % 2 == 0) {
-                lock.lock();
-                try {
-                    println("RoB run $i")
-                    conditionA.signal()
-                    conditionB.await()
-                } finally {
-                    lock.unlock();
-                }
-            }
-        }
-    }
-}
+
 
 /*
 val barrier = CyclicBarrier(2)
---
+++ Thread_I54W/app/src/main/java/ca/six/thread_i54w/biz/Print1To100InOrderWithTwoThread.kt
@@ -0,0 +1,61 @@
+package ca.six.thread_i54w.biz
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ca.six.thread_i54w.R
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.locks.Condition
+import java.util.concurrent.locks.Lock
+import java.util.concurrent.locks.ReentrantLock
+import java.util.function.Predicate
+
+class Print1To100InOrderWithTwoThread : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val lock = ReentrantLock()
+        val turnOfA = lock.newCondition()
+        val turnOfB = lock.newCondition()
+        val t1 = Thread(
+            RoPrint1To100(
+                lock,
+                myCondition = turnOfA,
+                anotherCondition = turnOfB
+            ) { num -> num % 2 == 1 })
+        val t2 = Thread(
+            RoPrint1To100(
+                lock,
+                myCondition = turnOfB,
+                anotherCondition = turnOfA
+            ) { num -> num % 2 == 0 })
+
+        btnMain.setOnClickListener {
+            t1.start()
+            t2.start()
+        }
+    }
+}
+
+
+class RoPrint1To100(
+    val lock: Lock,
+    val myCondition: Condition,
+    val anotherCondition: Condition,
+    val p: Predicate<Int>
+) : Runnable {
+    override fun run() {
+        for (i in 1..100) {
+            if (p.test(i)) {
+                lock.lock();
+                try {
+                    println("RoB run $i")
+                    anotherCondition.signal()
+                    myCondition.await()
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }
+    }
+}
