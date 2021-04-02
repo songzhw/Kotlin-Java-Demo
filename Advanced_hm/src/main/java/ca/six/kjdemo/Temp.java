8cf32eb84a431cf1f92152d864418c6e7a60a811
songzhw <songzhw2012@gmail.com>
2021-04-01 22:10:27 -0400

    to hanzi

--- Advanced_hm/src/main/java/ca/six/kjdemo/Temp.java
+++ Advanced_hm/src/main/java/ca/six/kjdemo/Temp.java
@@ -1,20 +1,9 @@
 package ca.six.kjdemo;
 
+class Worker {
+    String id = calculate();
 
-class A {
-
-}
-
-class B {
-    public static void main(String[] args) {
-        A a = new A();
-        foo(a);
-        boolean isNull = a == null;
-        System.out.println(isNull + "; " + a); //=> false;   ca.six.kjdemo.A@2c7b84de
-    }
-
-    public static void foo(A arg) {
-        System.out.println("B: " + arg); //=> B: ca.six.kjdemo.A@2c7b84de
-        arg = null;
+    public String calculate(){
+        return "myid";
     }
 }
--- Advanced_hm/src/main/kotlin/ca/six/kjdemo/temp.kt
+++ Advanced_hm/src/main/kotlin/ca/six/kjdemo/temp.kt
@@ -1,2 +1,17 @@
 package ca.six.kjdemo
 
+import java.util.regex.Pattern
+
+val from = "Android&#24320;&#21457;&#33402;&#26415;&#25506;&#32034;.epub"
+
+fun main() {
+    var result = from
+    val pattern = Pattern.compile("&#(\\d+);")
+    val matches = pattern.matcher(from)
+    while(matches.find()){
+        val hex = matches.group(1) //group(0)就是 "&#24320;", 而group(1)就是24320
+        val char = Integer.parseInt(hex, 10).toChar()
+        result = result.replace(matches.group(0), char.toString())
+    }
+    println(result)
+}
