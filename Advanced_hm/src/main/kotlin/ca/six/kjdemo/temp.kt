ff15a7f6d6b2f728b263b84e7b88b32f83aec42a
songzhw <songzhw2012@gmail.com>
2021-04-07 23:22:16 -0400

    no data at all

--- Advanced_hm/src/main/kotlin/ca/six/kjdemo/temp.kt
+++ Advanced_hm/src/main/kotlin/ca/six/kjdemo/temp.kt
@@ -1,11 +1,19 @@
 package ca.six.kjdemo
 
 import io.reactivex.Observable
+import java.util.concurrent.TimeUnit
 
 fun main() {
-    Observable.range(1, 8)
-        .groupBy { num -> if (num % 2 == 0) "even" else "odd" }
-        .filter{pair -> pair.key == "odd"}
-        .subscribe{ob -> ob.subscribe{pair -> println(pair)}}
+    Observable.interval(10, TimeUnit.MICROSECONDS)
+        .subscribe{num -> println(num)}
 }
 
+
+fun groupBy_Demo() {
+    // groupBy()得到的是一个 GroupedObservable(), 所以才要后面再用flatMap()
+    Observable.range(1, 8)
+        .groupBy { num -> if (num % 2 == 0) "even" else "odd" }
+        .filter { groupOb -> groupOb.key == "odd" }
+        .flatMap { it -> it }
+        .subscribe { num -> println(num) }
+}
