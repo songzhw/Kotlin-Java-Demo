4bc290700b3f6ef0abbf495809e33d38acb89d43
songzhw <songzhw2012@gmail.com>
2021-04-08 00:03:08 -0400

    custom operator

--- Advanced_hm/src/main/kotlin/ca/six/kjdemo/temp.kt
+++ Advanced_hm/src/main/kotlin/ca/six/kjdemo/temp.kt
@@ -1,21 +1,55 @@
 package ca.six.kjdemo
 
 import io.reactivex.Observable
+import io.reactivex.ObservableOperator
+import io.reactivex.Observer
+import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import java.util.concurrent.TimeUnit
 
 fun main() {
-    var temp = 0L
     Observable.interval(100, TimeUnit.MILLISECONDS, Schedulers.trampoline())
-        .filter{ num ->
-            if(num - temp >= 20) {
-                temp = num
-                true
-            } else {
-                false
+        .lift(Gap(20))
+        .subscribe { num -> println(num) } //=> 20, 40, 60, 80, 100, 120, ...
+}
+
+class Gap(val gap: Int) : ObservableOperator<Long, Long> {
+    override fun apply(observer: Observer<in Long>) = object : Observer<Long> {
+        private var temp = 0L
+        private lateinit var disposable: Disposable
+
+        override fun onSubscribe(d: Disposable) {
+            disposable = d
+            observer.onSubscribe(d)
+        }
+
+        override fun onNext(t: Long) {
+            if (disposable.isDisposed) {
+                return
+            }
+
+            if (t - temp >= gap) {
+                temp = t
+                observer.onNext(t)
             }
         }
-        .subscribe{num -> println(num)} //=> 20, 40, 60, 80, 100, 120, ...
+
+        override fun onError(e: Throwable) {
+            if (disposable.isDisposed) {
+                return
+            }
+            observer.onError(e)
+        }
+
+        override fun onComplete() {
+            if (disposable.isDisposed) {
+                return
+            }
+            observer.onComplete()
+        }
+
+    }
+
 }
 
 
