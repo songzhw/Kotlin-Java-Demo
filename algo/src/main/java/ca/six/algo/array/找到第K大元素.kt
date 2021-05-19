e04ce8b48877b66342d1a571a68a5aeb1ae8d562
songzhw <songzhw2012@gmail.com>
2021-05-18 21:48:06 -0400

    algo

--- algo/src/main/java/ca/six/algo/array/找到第K大元素.kt
+++ algo/src/main/java/ca/six/algo/array/找到第K大元素.kt
@@ -1,5 +1,8 @@
 package ca.six.algo.array
 
+import java.util.*
+import kotlin.collections.ArrayList
+
 // 一个无序数组中, 找到第K大的元素
 
 
@@ -18,14 +21,42 @@ fun k2(k: Int) {
     println(ary[k-1])
 }
 
+// 我们只关心第K大, 并不强求第K大之后的数组是无序还是有序的, 所以现在可以改O(nlogn)为O(n * K), 在数组极大时约等于O(n)
 fun k3(k: Int) {
     if (k <= 0) { return }
-    val ary = intArrayOf(23, 12, 38, 9, 45, 120, 46)
+    val ary = arrayListOf(23, 12, 38, 9, 45, 120, 46)
+
+
+    val maxNumbers = arrayListOf<Int>()
+    for(i in 1..k){
+        getMax(ary, maxNumbers)
+    }
+    println(maxNumbers[0])
+}
+
+private fun getMax(from: ArrayList<Int>, exception: ArrayList<Int>): Int {
+    var max = Int.MIN_VALUE
+    for(num in from){
+        if(num > max && !exception.contains(num)) {
+            max = num
+        }
+    }
+    exception.add(0, max)
+    return max
 }
 
 fun k4(k: Int) {
     if (k <= 0) { return }
     val ary = intArrayOf(23, 12, 38, 9, 45, 120, 46)
+    val heap = PriorityQueue<Int>(k) //java.util.PriorityQueue是最小堆
+    for(num in ary){
+        heap.add(num) //会重新排序的
+        if(heap.size > k){
+            heap.poll() //把最小值给弄出去
+        }
+    }
+    println(heap.peek()) //peek()就是最root值. 最小堆中就是最小值.
+    // 因为heap的size就是K, 所以这个peek()出的最小值, 就是整个数组的第K大元素
 }
 
 fun k5(k: Int) {
@@ -47,6 +78,6 @@ fun k7(k: Int) {
 
 
 fun main() {
-    k1(2) //=> 46
-    k1(5) //=> 23
+    k4(2) //=> 46
+    k4(5) //=> 23
 }
